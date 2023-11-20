a3c9bd499192a9b12aa82e6420039196733b31e5
squall7011 <gurdl7011@dsm.hs.kr>
2023-11-20 19:46:53 +0900

    📝 :: application.yml

--- maeumgagym-application/build.gradle.kts
+++ maeumgagym-application/build.gradle.kts
@@ -7,6 +7,9 @@ dependencies {
     // impl project
     implementation(project(":maeumgagym-core"))
 
+    // MySQL
+    implementation(Dependencies.MYSQL_CONNECTOR)
+
     // jpa
     implementation(Dependencies.SPRING_DATA_JPA)
 }
