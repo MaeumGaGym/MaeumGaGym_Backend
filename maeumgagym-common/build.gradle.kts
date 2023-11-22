54cb575999caeb9a49015e6bc295b2f9cae15d97
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-22 19:53:35 +0900

    ➕ :: 의존성 추가

--- maeumgagym-common/build.gradle.kts
+++ maeumgagym-common/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    implementation(Dependencies.SPRING_WEB)
 }
 
 tasks.getByName<Test>("test") {
