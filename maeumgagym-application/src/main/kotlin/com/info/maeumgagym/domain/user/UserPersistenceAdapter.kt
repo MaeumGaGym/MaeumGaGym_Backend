7237a7d45b9a14aeda914d48aae70d2b774904ae
HyunSu1768 <azxcv1768@gmail.com>
2023-11-28 18:50:14 +0900

    ✨ :: Add PersistenceAdapater

--- maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/user/UserPersistenceAdapter.kt
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/user/UserPersistenceAdapter.kt
@@ -2,12 +2,22 @@ package com.info.maeumgagym.domain.user
 
 import com.info.common.PersistenceAdapter
 import com.info.maeumgagym.user.model.User
+import com.info.maeumgagym.user.port.out.CreateUserPort
+import com.info.maeumgagym.user.port.out.FindUserByOAuthIdPort
 import com.info.maeumgagym.user.port.out.FindUserPort
 import java.util.*
 
 @PersistenceAdapter
-class UserPersistenceAdapter() : FindUserPort {
+class UserPersistenceAdapter() : FindUserPort, CreateUserPort, FindUserByOAuthIdPort {
     override fun findUserById(userId: UUID): User {
+        TODO("Not yet implemented")
+    }
+
+    override fun createUser(user: User): User {
+        TODO("Not yet implemented")
+    }
+
+    override fun findUserByOAuthId(oauthId: String): User? {
         TODO("Not yet implemented")
     }
 }
