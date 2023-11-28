dc0e0a3d4cdc06b8fdac7bdc3b36d4440882c190
HyunSu1768 <azxcv1768@gmail.com>
2023-11-28 18:48:48 +0900

    ✨ :: Add KakaoAuthController

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
@@ -1,9 +1,20 @@
 package com.info.maeumgagym.auth.controller
 
+import com.info.maeumgagym.auth.dto.response.TokenResponse
 import com.info.maeumgagym.auth.port.`in`.GetKakaoAccessTokenUseCase
+import com.info.maeumgagym.auth.port.`in`.KakaoLoginUseCase
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class KakaoAuthController(
-    private val getKakaoAccessTokenUseCase: GetKakaoAccessTokenUseCase
-)
+    private val getKakaoAccessTokenUseCase: GetKakaoAccessTokenUseCase,
+    private val kakaoLoginUseCase: KakaoLoginUseCase
+) {
+    @GetMapping("/app/login/kakao")
+    fun login(@RequestParam("code") code: String): TokenResponse {
+        val kakaoTokenResponse = getKakaoAccessTokenUseCase.getAccessToken(code)
+        return kakaoLoginUseCase.login(kakaoTokenResponse.accessToken)
+    }
+}
