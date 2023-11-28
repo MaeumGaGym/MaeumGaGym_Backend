76d3d91cf77d3a1a1df1089e9562192b4d68421e
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-11-28 17:04:30 +0900

    ♻ :: Adapter 합체

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/adapter/GoogleAuthAdapter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/adapter/GoogleAuthAdapter.kt
@@ -1,18 +1,22 @@
 package com.info.maeumgagym.auth.adapter
 
+import com.info.common.WebAdapter
+import com.info.maeumgagym.auth.google.dto.response.GoogleInfoResponse
 import com.info.maeumgagym.auth.google.dto.response.GoogleTokenResponse
 import com.info.maeumgagym.auth.port.out.GetGoogleAccessTokenPort
+import com.info.maeumgagym.auth.port.out.GetGoogleInfoPort
 import com.info.maeumgagym.feign.oauth.google.GoogleAuthClient
+import com.info.maeumgagym.feign.oauth.google.GoogleInfoClient
 import com.info.maeumgagym.global.env.BaseProperty
 import com.info.maeumgagym.global.env.google.GoogleProperty
-import org.springframework.stereotype.Component
 
-@Component
-class GetGoogleAccessTokenAdapter(
+@WebAdapter
+class GoogleAuthAdapter(
     private val baseProperty: BaseProperty,
     private val googleProperty: GoogleProperty,
-    private val googleAuthClient: GoogleAuthClient
-) : GetGoogleAccessTokenPort {
+    private val googleAuthClient: GoogleAuthClient,
+    private val googleInfoClient: GoogleInfoClient
+) : GetGoogleAccessTokenPort, GetGoogleInfoPort {
 
     override fun getGoogleAccessToken(code: String): GoogleTokenResponse =
         googleAuthClient.googleAuth(
@@ -21,5 +25,11 @@ class GetGoogleAccessTokenAdapter(
             clientSecret = googleProperty.clientSecret,
             redirectUri = baseProperty.url + googleProperty.redirectUrl,
             grantType = googleProperty.grantType
+        )
+
+    override fun getGoogleInfo(accessToken: String): GoogleInfoResponse =
+        googleInfoClient.googleInfo(
+            "json",
+            accessToken
         )
 }
