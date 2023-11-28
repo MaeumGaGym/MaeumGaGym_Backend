0e4f5a206c3d2b8319ee6a00d7c259847e3dbbbe
HyunSu1768 <azxcv1768@gmail.com>
2023-11-28 20:07:32 +0900

    ✨ :: Add UserRepository

---
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/user/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.info.maeumgagym.domain.user.repository
+
+import com.info.maeumgagym.domain.user.entity.UserJpaEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface UserRepository : JpaRepository<UserJpaEntity, UUID> {
+    fun findByOauthId(oauthId: String): UserJpaEntity?
+}
