839fcb47f4d1aecca4860b5c038d7e1e98ae1077
HyunSu1768 <azxcv1768@gmail.com>
2023-12-01 16:32:11 +0900

    ♻️ :: rename

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/auth/service/KakaoLoginService.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/auth/service/KakaoLoginService.kt
@@ -7,7 +7,7 @@ import com.info.maeumgagym.auth.port.out.GenerateJwtPort
 import com.info.maeumgagym.auth.port.out.GetKakaoInfoPort
 import com.info.maeumgagym.user.model.Role
 import com.info.maeumgagym.user.model.User
-import com.info.maeumgagym.user.port.out.CreateUserPort
+import com.info.maeumgagym.user.port.out.SaveUserPort
 import com.info.maeumgagym.user.port.out.FindUserByOAuthIdPort
 
 @UseCase
@@ -15,11 +15,11 @@ class KakaoLoginService(
     private val getKakaoInfoPort: GetKakaoInfoPort,
     private val findUserByOAuthIdPort: FindUserByOAuthIdPort,
     private val generateJwtPort: GenerateJwtPort,
-    private val createUserPort: CreateUserPort
+    private val saveUserPort: SaveUserPort
 ) : KakaoLoginUseCase {
     override fun login(accessToken: String): TokenResponse {
         val userInfo = getKakaoInfoPort.getInfo(accessToken)
-        val user = findUserByOAuthIdPort.findUserByOAuthId(userInfo.id) ?: createUserPort.createUser(
+        val user = findUserByOAuthIdPort.findUserByOAuthId(userInfo.id) ?: saveUserPort.saveUser(
             User(
                 nickname = userInfo.properties.nickname,
                 roles = mutableListOf(Role.USER, Role.ADMIN),
