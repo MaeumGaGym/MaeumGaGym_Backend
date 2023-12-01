b2ec90aaec10f3793ed1b98390425a62f195c906
HyunSu1768 <azxcv1768@gmail.com>
2023-12-01 14:58:19 +0900

    ♻️ :: Delete Get AccessToken

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/adapter/KakaoAuthAdapter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/adapter/KakaoAuthAdapter.kt
@@ -1,17 +1,13 @@
 package com.info.maeumgagym.auth.adapter
 
 import com.info.maeumgagym.auth.dto.response.KakaoInfoResponse
-import com.info.maeumgagym.auth.dto.response.KakaoTokenResponse
-import com.info.maeumgagym.auth.port.out.GetKakaoAccessTokenPort
 import com.info.maeumgagym.auth.port.out.GetKakaoInfoPort
-import com.info.maeumgagym.global.env.kakao.KakaoProperties
 import com.info.maeumgagym.feign.oauth.kakao.KakaoInfoClient
 import org.springframework.stereotype.Component
 
 @Component
 class KakaoAuthAdapter(
-    private val kakaoInfoClient: KakaoInfoClient,
-    private val kakaoProperties: KakaoProperties
+    private val kakaoInfoClient: KakaoInfoClient
 ) : GetKakaoInfoPort {
     override fun getInfo(accessToken: String): KakaoInfoResponse = kakaoInfoClient.kakaoInfo("Bearer $accessToken")
 }
--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
@@ -1,16 +1,13 @@
 package com.info.maeumgagym.auth.controller
 
 import com.info.maeumgagym.auth.dto.response.TokenResponse
-import com.info.maeumgagym.auth.port.`in`.GetKakaoAccessTokenUseCase
 import com.info.maeumgagym.auth.port.`in`.KakaoLoginUseCase
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
 
 @Validated
 @RequestMapping("/kakao")
