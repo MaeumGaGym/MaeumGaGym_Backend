00c8c6222db2d86c32bb544a32128f826910d046
HyunSu1768 <azxcv1768@gmail.com>
2023-12-01 16:31:28 +0900

    ♻️ :: rename

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/user/port/out/FindUserByUUIDPort.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/user/port/out/FindUserByUUIDPort.kt
@@ -3,6 +3,6 @@ package com.info.maeumgagym.user.port.out
 import com.info.maeumgagym.user.model.User
 import java.util.UUID
 
-interface FindUserPort {
-    fun findUserById(userId: UUID): User
+interface FindUserByUUIDPort {
+    fun findUserById(userId: UUID): User?
 }
