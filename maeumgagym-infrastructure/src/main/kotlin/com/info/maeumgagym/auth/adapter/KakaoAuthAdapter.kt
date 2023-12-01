8229a20e746ffbd05bfb650d10289e9ad4bd0937
HyunSu1768 <azxcv1768@gmail.com>
2023-12-01 14:54:03 +0900

    ♻️ :: GetAccessToken 삭제

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/adapter/KakaoAuthAdapter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/adapter/KakaoAuthAdapter.kt
@@ -5,24 +5,13 @@ import com.info.maeumgagym.auth.dto.response.KakaoTokenResponse
 import com.info.maeumgagym.auth.port.out.GetKakaoAccessTokenPort
 import com.info.maeumgagym.auth.port.out.GetKakaoInfoPort
 import com.info.maeumgagym.global.env.kakao.KakaoProperties
-import com.info.maeumgagym.feign.oauth.kakao.KakaoAuthClient
 import com.info.maeumgagym.feign.oauth.kakao.KakaoInfoClient
 import org.springframework.stereotype.Component
 
 @Component
 class KakaoAuthAdapter(
-    private val kakaoAuthClient: KakaoAuthClient,
     private val kakaoInfoClient: KakaoInfoClient,
     private val kakaoProperties: KakaoProperties
-) : GetKakaoAccessTokenPort, GetKakaoInfoPort {
-    override fun getAccessToken(code: String): KakaoTokenResponse {
-        return kakaoAuthClient.kakaoAuth(
-            grantType = kakaoProperties.grantType,
-            clientId = kakaoProperties.clientId,
-            redirectUri = kakaoProperties.redirectUri,
-            code = code
-        )
-    }
-
+) : GetKakaoInfoPort {
     override fun getInfo(accessToken: String): KakaoInfoResponse = kakaoInfoClient.kakaoInfo("Bearer $accessToken")
 }
--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
@@ -3,18 +3,23 @@ package com.info.maeumgagym.auth.controller
 import com.info.maeumgagym.auth.dto.response.TokenResponse
 import com.info.maeumgagym.auth.port.`in`.GetKakaoAccessTokenUseCase
 import com.info.maeumgagym.auth.port.`in`.KakaoLoginUseCase
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
+@Validated
+@RequestMapping("/kakao")
 @RestController
 class KakaoAuthController(
-    private val getKakaoAccessTokenUseCase: GetKakaoAccessTokenUseCase,
     private val kakaoLoginUseCase: KakaoLoginUseCase
 ) {
-    @GetMapping("/app/login/kakao")
-    fun login(@RequestParam("code") code: String): TokenResponse {
-        val kakaoTokenResponse = getKakaoAccessTokenUseCase.getAccessToken(code)
-        return kakaoLoginUseCase.login(kakaoTokenResponse.accessToken)
+    @PostMapping("/login")
+    fun login(@RequestParam("accessToken", required = true) @NotBlank accessToken: String?): TokenResponse {
+        return kakaoLoginUseCase.login(accessToken!!)
     }
 }
