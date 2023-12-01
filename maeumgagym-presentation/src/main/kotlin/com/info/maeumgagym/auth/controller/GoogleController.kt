7d267a74083ea7f5849e5f7df4f401f8382a4b1f
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-12-01 15:13:35 +0900

    ♻ :: PostMapping으로 변경

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/GoogleController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/GoogleController.kt
@@ -2,7 +2,7 @@ package com.info.maeumgagym.auth.controller
 
 import com.info.maeumgagym.auth.port.`in`.GoogleLoginUseCase
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -15,7 +15,7 @@ class GoogleController(
     private val googleLoginService: GoogleLoginUseCase
 ) {
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     fun login(@RequestParam(required = true) @NotNull accessToken: String?) {
         googleLoginService.googleLogin(accessToken!!)
     }
