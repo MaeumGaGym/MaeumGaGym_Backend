5e47dcf66dd65ae6ed44144755847ad5fdc009ff
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-12-01 19:40:00 +0900

    ✨ :: refreshToken 추가

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/jwt/JwtAdapter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/jwt/JwtAdapter.kt
@@ -2,21 +2,21 @@ package com.info.maeumgagym.global.jwt
 
 import com.info.maeumgagym.auth.dto.response.TokenResponse
 import com.info.maeumgagym.auth.port.out.GenerateJwtPort
+import com.info.maeumgagym.auth.port.out.ParsePublicKeyPort
 import com.info.maeumgagym.auth.port.out.ReadCurrentUserPort
 import com.info.maeumgagym.domain.user.exception.UserNotFoundException
 import com.info.maeumgagym.global.env.jwt.JwtProperties
 import com.info.maeumgagym.global.exception.ExpiredTokenException
 import com.info.maeumgagym.global.exception.InvalidTokenException
-import com.info.maeumgagym.user.model.User
-import com.info.maeumgagym.user.port.out.FindUserByUUIDPort
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.stereotype.Component
-import com.info.maeumgagym.auth.port.out.ParsePublicKeyPort
 import com.info.maeumgagym.global.security.principle.CustomUserDetailService
 import com.info.maeumgagym.global.security.principle.CustomUserDetails
+import com.info.maeumgagym.user.model.User
+import com.info.maeumgagym.user.port.out.FindUserByUUIDPort
 import io.jsonwebtoken.*
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
 import java.security.PublicKey
 import java.util.*
 
@@ -29,6 +29,7 @@ class JwtAdapter(
     override fun generateToken(subject: Any): TokenResponse {
         return TokenResponse(
             generateAccessToken(subject),
+            generateRefreshToken()
         )
     }
 
@@ -38,6 +39,15 @@ class JwtAdapter(
             .setSubject(subject.toString())
             .setIssuedAt(now)
             .setExpiration(Date(now.time + jwtProperties.accessExpiredExp * 1000L))
+            .signWith(SignatureAlgorithm.HS256, jwtProperties.secretKey)
+            .compact()
+    }
+
+    private fun generateRefreshToken(): String {
+        val now = Date()
+        return Jwts.builder()
+            .setIssuedAt(now)
+            .setExpiration(Date(now.time + jwtProperties.refreshExpiredExp * 1000L))
             .signWith(SignatureAlgorithm.HS256, jwtProperties.secretKey)
             .compact()
     }
