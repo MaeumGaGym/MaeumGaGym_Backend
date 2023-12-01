e94d1c7d16204f316c9639ec9b456d822ad58bb9
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-12-01 15:19:15 +0900

    🔥 :: UseCase 제거

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/auth/port/in/GetKakaoAccessTokenUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.info.maeumgagym.auth.port.`in`
-
-import com.info.maeumgagym.auth.dto.response.KakaoTokenResponse
-
-interface GetKakaoAccessTokenUseCase {
-    fun getAccessToken(code: String): KakaoTokenResponse
-}
