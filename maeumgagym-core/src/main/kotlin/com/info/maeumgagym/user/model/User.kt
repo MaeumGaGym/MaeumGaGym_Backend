c2e68a39ca9934167f3d802c186797ccf1b4872b
HyunSu1768 <azxcv1768@gmail.com>
2023-12-01 16:31:41 +0900

    ♻️ :: usermodel

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/user/model/User.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/user/model/User.kt
@@ -7,5 +7,6 @@ data class User(
     val nickname: String,
     val roles: MutableList<Role> = mutableListOf(),
     val oauthId: String,
-    val profilePath: String? = null
+    val profilePath: String? = null,
+    val isDeleted: Boolean = false
 )
