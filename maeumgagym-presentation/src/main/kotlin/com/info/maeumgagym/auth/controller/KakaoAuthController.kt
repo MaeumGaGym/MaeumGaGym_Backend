892d243f39c4cee05b638b0f8da8b435fe3177c3
HyunSu1768 <azxcv1768@gmail.com>
2023-12-04 20:17:35 +0900

    ✨ :: Add KakaoDuplicateCheckUser API

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
@@ -2,10 +2,12 @@ package com.info.maeumgagym.auth.controller
 
 import com.info.maeumgagym.auth.controller.dto.request.KakaoSignupRequest
 import com.info.maeumgagym.auth.dto.response.TokenResponse
+import com.info.maeumgagym.auth.port.`in`.KakaoDuplicateCheckUseCase
 import com.info.maeumgagym.auth.port.`in`.KakaoLoginUseCase
 import com.info.maeumgagym.auth.port.`in`.KakaoSignupUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,7 +21,8 @@ import javax.validation.constraints.NotBlank
 @RestController
 class KakaoAuthController(
     private val kakaoLoginUseCase: KakaoLoginUseCase,
-    private val kakaoSignupUseCase: KakaoSignupUseCase
+    private val kakaoSignupUseCase: KakaoSignupUseCase,
+    private val kakaoDuplicateCheckUseCase: KakaoDuplicateCheckUseCase
 ) {
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
@@ -36,4 +39,8 @@ class KakaoAuthController(
         accessToken!!,
         kakaoSignupRequest.nickname
     )
+
+    @GetMapping
+    fun duplicatCheck(@RequestParam("access_token", required = true) @NotBlank accessToken: String?): Boolean =
+        kakaoDuplicateCheckUseCase.duplicateCheck(accessToken!!)
 }
