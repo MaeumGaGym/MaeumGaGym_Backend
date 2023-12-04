78f2624cdf2fe97d1b29c7503ba0d4a236a404ef
HyunSu1768 <azxcv1768@gmail.com>
2023-12-04 17:22:53 +0900

    ♻️ :: ktlint

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/auth/service/KakaoLoginService.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/auth/service/KakaoLoginService.kt
@@ -7,16 +7,13 @@ import com.info.maeumgagym.auth.exception.UserNotFoundException
 import com.info.maeumgagym.auth.port.`in`.KakaoLoginUseCase
 import com.info.maeumgagym.auth.port.out.GenerateJwtPort
 import com.info.maeumgagym.auth.port.out.GetKakaoInfoPort
-import com.info.maeumgagym.user.model.Role
-import com.info.maeumgagym.user.model.User
-import com.info.maeumgagym.user.port.out.SaveUserPort
 import com.info.maeumgagym.user.port.out.FindUserByOAuthIdPort
 
 @UseCase
 class KakaoLoginService(
     private val getKakaoInfoPort: GetKakaoInfoPort,
     private val findUserByOAuthIdPort: FindUserByOAuthIdPort,
-    private val generateJwtPort: GenerateJwtPort,
+    private val generateJwtPort: GenerateJwtPort
 ) : KakaoLoginUseCase {
     override fun login(accessToken: String): TokenResponse {
         val userInfo = getKakaoInfoPort.getInfo(accessToken)
