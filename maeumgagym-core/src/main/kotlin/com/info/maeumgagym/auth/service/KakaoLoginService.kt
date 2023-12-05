8326ccdcc13a380e50e1f33ca3e510e4648ab30f
HyunSu1768 <azxcv1768@gmail.com>
2023-12-05 16:57:19 +0900

    ♻️ :: Change Login Logic

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/auth/service/KakaoLoginService.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/auth/service/KakaoLoginService.kt
@@ -2,28 +2,21 @@ package com.info.maeumgagym.auth.service
 
 import com.info.common.UseCase
 import com.info.maeumgagym.auth.dto.response.TokenResponse
-import com.info.maeumgagym.auth.exception.AlreadyWithdrawalUserException
 import com.info.maeumgagym.auth.exception.UserNotFoundException
 import com.info.maeumgagym.auth.port.`in`.KakaoLoginUseCase
 import com.info.maeumgagym.auth.port.out.GenerateJwtPort
 import com.info.maeumgagym.auth.port.out.GetKakaoInfoPort
-import com.info.maeumgagym.user.port.out.FindUserByOAuthIdPort
+import com.info.maeumgagym.user.port.out.ExistUserByOAuthIdPort
 
 @UseCase
 class KakaoLoginService(
     private val getKakaoInfoPort: GetKakaoInfoPort,
-    private val findUserByOAuthIdPort: FindUserByOAuthIdPort,
-    private val generateJwtPort: GenerateJwtPort
+    private val generateJwtPort: GenerateJwtPort,
+    private val existUserByOAuthIdPort: ExistUserByOAuthIdPort
 ) : KakaoLoginUseCase {
     override fun login(accessToken: String): TokenResponse {
         val userInfo = getKakaoInfoPort.getInfo(accessToken)
-        val user = findUserByOAuthIdPort.findUserByOAuthId(userInfo.id)?.let {
-            if (it.isDeleted) {
-                throw AlreadyWithdrawalUserException
-            } else {
-                return@let it
-            }
-        } ?: throw UserNotFoundException
-        return generateJwtPort.generateToken(user.id.toString())
+        if (!existUserByOAuthIdPort.existByOAuthId(userInfo.id)) throw UserNotFoundException
+        return generateJwtPort.generateToken(userInfo.id)
     }
 }
--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/auth/controller/KakaoAuthController.kt
@@ -2,12 +2,10 @@ package com.info.maeumgagym.auth.controller
 
 import com.info.maeumgagym.auth.controller.dto.request.KakaoSignupRequest
 import com.info.maeumgagym.auth.dto.response.TokenResponse
-import com.info.maeumgagym.auth.port.`in`.KakaoDuplicateCheckUseCase
 import com.info.maeumgagym.auth.port.`in`.KakaoLoginUseCase
 import com.info.maeumgagym.auth.port.`in`.KakaoSignupUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,8 +19,7 @@ import javax.validation.constraints.NotBlank
 @RestController
 class KakaoAuthController(
     private val kakaoLoginUseCase: KakaoLoginUseCase,
-    private val kakaoSignupUseCase: KakaoSignupUseCase,
-    private val kakaoDuplicateCheckUseCase: KakaoDuplicateCheckUseCase
+    private val kakaoSignupUseCase: KakaoSignupUseCase
 ) {
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
@@ -39,8 +36,4 @@ class KakaoAuthController(
         accessToken!!,
         kakaoSignupRequest.nickname
     )
-
-    @GetMapping
-    fun duplicatCheck(@RequestParam("access_token", required = true) @NotBlank accessToken: String?): Boolean =
-        kakaoDuplicateCheckUseCase.duplicateCheck(accessToken!!)
 }
