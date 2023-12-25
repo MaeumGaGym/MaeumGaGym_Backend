daaf44e4fb8eb42186413c27bc9c6d6c5b46734e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-12-25 23:08:21 +0900

    ⭐️ :: 피클 수정

---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pickle/dto/request/UpdatePickleRequest.kt
@@ -0,0 +1,18 @@
+package com.info.maeumgagym.pickle.dto.request
+
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
+
+data class UpdatePickleRequest(
+
+    @field:NotBlank(message = "null일 수 없습니다")
+    val title: String?,
+
+    val description: String?,
+
+    val tags: MutableSet<String> = mutableSetOf(),
+
+    @field:NotBlank(message = "null일 수 없습니다")
+    @field:Pattern(regexp = "^(https://[\\w.-]+)/\\d{10}/m3u8.index", message = "올바른 url이 아닙니다")
+    val videoUrl: String?
+)
