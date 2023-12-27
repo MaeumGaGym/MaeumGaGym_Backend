b88e1e811e81f0a0e677b15c94e53adfda63164c
HyunSu1768 <azxcv1768@gmail.com>
2023-12-27 22:44:32 +0900

    ♻️ :: ktlint

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleController.kt
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
 
