596199e9cd24b93d74d89436c06bed71462886bc
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-12-28 19:07:10 +0900

    📝 :: lint

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pose/PoseController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pose/PoseController.kt
@@ -9,7 +9,6 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "Pose API")
 @RequestMapping("/poses")
