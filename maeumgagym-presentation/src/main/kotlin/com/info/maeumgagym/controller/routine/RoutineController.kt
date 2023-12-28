38978126bd26311ba1ef390efa81760fb6762c2d
HyunSu1768 <azxcv1768@gmail.com>
2023-12-28 22:06:10 +0900

    ✨ :: Add Routine Delete API

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/RoutineController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/RoutineController.kt
@@ -4,21 +4,27 @@ import com.info.common.WebAdapter
 import com.info.maeumgagym.controller.routine.dto.CreateRoutineWebRequest
 import com.info.maeumgagym.controller.routine.dto.RoutineWebListResponse
 import com.info.maeumgagym.routine.port.`in`.CreateRoutineUseCase
+import com.info.maeumgagym.routine.port.`in`.DeleteRoutineUseCase
 import com.info.maeumgagym.routine.port.`in`.ReadAllMyRoutineUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import java.util.*
 import javax.validation.Valid
+import javax.validation.constraints.NotNull
 
 @Tag(name = "Routine API")
 @RequestMapping("/routines")
 @WebAdapter
 class RoutineController(
     private val createRoutineUseCase: CreateRoutineUseCase,
-    private val readAllMyRoutineUseCase: ReadAllMyRoutineUseCase
+    private val readAllMyRoutineUseCase: ReadAllMyRoutineUseCase,
+    private val deleteRoutineUseCase: DeleteRoutineUseCase
 ) {
     @Operation(summary = "루틴 생성 API")
     @PostMapping
@@ -34,4 +40,14 @@ class RoutineController(
     fun readAllMyRoutine(): RoutineWebListResponse = RoutineWebListResponse.toWebResponse(
         readAllMyRoutineUseCase.readAllMyRoutine()
     )
+
+    @Operation(summary = "루틴 삭제 API")
+    @DeleteMapping("/{routineId}")
+    fun deleteRoutine(
+        @PathVariable(required = true)
+        @Valid
+        @NotNull(message = "null일 수 없습니다.")
+        routineId: UUID?
+    ) =
+        deleteRoutineUseCase.deleteRoutine(routineId!!)
 }
