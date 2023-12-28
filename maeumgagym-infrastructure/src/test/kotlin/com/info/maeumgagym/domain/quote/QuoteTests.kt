60b54fdcef5ee50d7db926d8b779f72d7241a0ad
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-12-28 19:49:08 +0900

    ㅋㅌㅊㅋㅍ

--- maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/QuoteTests.kt
+++ maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/QuoteTests.kt
@@ -1,7 +1,7 @@
 package com.info.maeumgagym.domain.quote
 
 import com.info.maeumgagym.domain.quote.exception.MismatchQuoteAndQuoterException
-import com.info.maeumgagym.quote.port.`in`.ReadRandomQuoteUseCase
+import com.info.maeumgagym.quote.service.ReadRandomQuoteService
 import com.info.maeumgagym.quote.vo.QuoteValueObject
 import com.info.maeumgagym.quote.vo.Quotes
 import org.junit.jupiter.api.Assertions
@@ -11,14 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class QuoteTests @Autowired constructor(
-    private val readRandomQuoteUseCase: ReadRandomQuoteUseCase
+    private val readRandomQuoteService: ReadRandomQuoteService
 ) {
 
     @Test
     fun getRandomQuoteAndContainedInQUOTESTest() {
         Assertions.assertDoesNotThrow {
             for (i in 1..10) {
-                val quoteResponse = readRandomQuoteUseCase.readRandomQuote()
+                val quoteResponse = readRandomQuoteService.readRandomQuote()
                 if (!Quotes.QUOTES.contains(
                         QuoteValueObject(quoteResponse.quote, quoteResponse.quoter)
                     )
