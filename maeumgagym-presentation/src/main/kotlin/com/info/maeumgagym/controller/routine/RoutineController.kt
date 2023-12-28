76bc427d7d12a3f0fe67f4a870ededca8558b637
HyunSu1768 <azxcv1768@gmail.com>
2023-12-28 22:10:51 +0900

    ♻️ :: Add import

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/RoutineController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/RoutineController.kt
@@ -9,11 +9,7 @@ import com.info.maeumgagym.routine.port.`in`.ReadAllMyRoutineUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 import java.util.*
 import javax.validation.Valid
 import javax.validation.constraints.NotNull
