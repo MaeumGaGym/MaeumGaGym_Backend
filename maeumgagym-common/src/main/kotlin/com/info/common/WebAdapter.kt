23ae3c72d831f051f6298a4f39cf9158d8fb569c
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-12-28 20:03:27 +0900

    ㄹㅇ

--- maeumgagym-common/src/main/kotlin/com/info/common/WebAdapter.kt
+++ maeumgagym-common/src/main/kotlin/com/info/common/WebAdapter.kt
@@ -1,7 +1,6 @@
 package com.info.common
 
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @Target(AnnotationTarget.CLASS)
