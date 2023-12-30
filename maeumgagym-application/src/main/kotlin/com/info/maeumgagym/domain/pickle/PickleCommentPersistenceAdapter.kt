7f685d9fed3254ced1206a323c644e6875737454
HyunSu1768 <azxcv1768@gmail.com>
2023-12-30 10:32:38 +0900

    ✨ :: PickleComment PersistenceAdapter

---
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/pickle/PickleCommentPersistenceAdapter.kt
@@ -0,0 +1,23 @@
+package com.info.maeumgagym.domain.pickle
+
+import com.info.common.PersistenceAdapter
+import com.info.maeumgagym.domain.pickle.mapper.PickleCommentMapper
+import com.info.maeumgagym.domain.pickle.repository.PickleCommentRepository
+import com.info.maeumgagym.pickle.model.PickleComment
+import com.info.maeumgagym.pickle.port.out.ReadPickleCommentPort
+import com.info.maeumgagym.pickle.port.out.SavePickleCommentPort
+import org.springframework.data.repository.findByIdOrNull
+
+@PersistenceAdapter
+class PickleCommentPersistenceAdapter(
+    private val pickleCommentRepository: PickleCommentRepository,
+    private val pickleCommentMapper: PickleCommentMapper
+): SavePickleCommentPort, ReadPickleCommentPort {
+    override fun readPickleComment(pickleCommentId: Long): PickleComment? =
+        pickleCommentRepository.findByIdOrNull(pickleCommentId)?.let { pickleCommentMapper.toDomain(it) }
+
+    override fun savePickleComment(pickleComment: PickleComment): PickleComment {
+        pickleCommentRepository.save(pickleCommentMapper.toEntity(pickleComment))
+        return pickleComment
+    }
+}
