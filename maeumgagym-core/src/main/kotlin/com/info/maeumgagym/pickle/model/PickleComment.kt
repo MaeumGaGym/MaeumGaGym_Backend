30a6733479a7b5c49fb0b9c9d5ce5ad5b306faf7
HyunSu1768 <azxcv1768@gmail.com>
2023-12-30 10:32:23 +0900

    ✨ :: PickleComment Entity, Model

---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pickle/model/PickleComment.kt
@@ -0,0 +1,14 @@
+package com.info.maeumgagym.pickle.model
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class PickleComment(
+    val content: String,
+    val pickleId: String,
+    val writerId: UUID,
+    val parentComment: PickleComment?,
+    val children: MutableList<PickleComment>? = mutableListOf(),
+    val createdAt: LocalDateTime? = null,
+    val isDeleted: Boolean = false
+)
