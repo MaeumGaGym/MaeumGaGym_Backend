73c773afbebc297f35ad4100644d43662a5f75f5
HyunSu1768 <azxcv1768@gmail.com>
2023-12-30 10:33:11 +0900

    ✨ :: PickleComment UseCase

---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pickle/port/in/CreatePickleCommentUseCase.kt
@@ -0,0 +1,7 @@
+package com.info.maeumgagym.pickle.port.`in`
+
+import com.info.maeumgagym.pickle.dto.request.PickleCommentRequest
+
+interface CreatePickleCommentUseCase {
+    fun createPickleComment(pickleCommentRequest: PickleCommentRequest)
+}
