9153d91659cf3a15f89a77f7433d4c1a37f73146
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-01 23:27:54 +0900

    ♻ :: 어노테이션 관련 리팩토링

--- maeumgagym-common/src/main/kotlin/com/info/common/UseCase.kt
+++ maeumgagym-common/src/main/kotlin/com/info/common/UseCase.kt
@@ -1,11 +1,12 @@
 package com.info.common
 
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 @MustBeDocumented
 @Transactional
-@Component
+@Service
 annotation class UseCase()
--- maeumgagym-common/src/main/kotlin/com/info/common/WebAdapter.kt
+++ maeumgagym-common/src/main/kotlin/com/info/common/WebAdapter.kt
@@ -7,5 +7,4 @@ import org.springframework.web.bind.annotation.RestController
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @RestController
-@Component
 annotation class WebAdapter()
