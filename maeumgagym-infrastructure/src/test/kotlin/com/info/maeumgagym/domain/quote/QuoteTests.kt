8e339de09dd0ec19216f68afa641185068c929e5
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-02 23:27:03 +0900

    ✨ :: JUnit 기반 루틴 삭제 테스트 추가

--- maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/QuoteTests.kt
+++ maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/QuoteTests.kt
@@ -7,10 +7,10 @@ import com.info.maeumgagym.quote.vo.Quotes
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 
-@AutoConfigureMockMvc
+@Transactional
 @SpringBootTest
 class QuoteTests @Autowired constructor(
     private val readRandomQuoteService: ReadRandomQuoteService
