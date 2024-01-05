94a30d9743ec98d7ff1c82436506a1a203f8bb45
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-05 18:56:07 +0900

    🌟 :: 어노테이션 변경

--- maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/junit5/QuoteTests.kt
+++ maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/junit5/QuoteTests.kt
@@ -5,7 +5,7 @@ import com.info.maeumgagym.quote.service.ReadRandomQuoteService
 import com.info.maeumgagym.quote.vo.QuoteValueObject
 import com.info.maeumgagym.quote.vo.Quotes
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.RepeatedTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
@@ -16,16 +16,15 @@ class QuoteTests @Autowired constructor(
     private val readRandomQuoteService: ReadRandomQuoteService
 ) {
 
-    @Test
+    @RepeatedTest(value = 10)
     fun getRandomQuoteAndContainedInQUOTESTest() {
         Assertions.assertDoesNotThrow {
-            for (i in 1..10) {
-                val quoteResponse = readRandomQuoteService.readRandomQuote()
-                if (!Quotes.QUOTES.contains(
-                        QuoteValueObject(quoteResponse.quote, quoteResponse.quoter)
-                    )
-                ) throw MismatchQuoteAndQuoterException
-            }
+            val quoteResponse = readRandomQuoteService.readRandomQuote()
+            if (!Quotes.QUOTES.contains(
+                    QuoteValueObject(quoteResponse.quote, quoteResponse.quoter)
+                )
+            ) throw MismatchQuoteAndQuoterException
+
         }
     }
 }
