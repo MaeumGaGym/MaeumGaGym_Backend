10b61a22bc8fc79a3adcead3bff0d4f50dc49d69
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-07 23:25:58 +0900

    ✨:: WIthdrawalUserServiceTests 추가

--- maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/junit5/QuoteTests.kt
+++ maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/domain/quote/junit5/QuoteTests.kt
@@ -24,7 +24,6 @@ class QuoteTests @Autowired constructor(
                     QuoteValueObject(quoteResponse.quote, quoteResponse.quoter)
                 )
             ) throw MismatchQuoteAndQuoterException
-
         }
     }
 }
