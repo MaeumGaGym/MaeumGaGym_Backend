4474e3726c7e5d2122bed9b9e6e538337f1a2068
HyunSu1768 <azxcv1768@gmail.com>
2024-01-10 08:30:13 +0900

    ♻️ :: validation

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleCommentController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleCommentController.kt
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @Tag(name = "Pickle Comment API")
 @Validated
@@ -39,11 +41,11 @@ class PickleCommentController(
     fun createPickleReplyComment(
         @RequestBody @Valid
         req: PickleCommentWebRequest,
-        @PathVariable
+        @PathVariable(value = "pickleId")
         @NotBlank(message = "pickleId는 null일 수 없습니다.")
         pickleId: String?,
-        @PathVariable
-        @NotBlank(message = "parentId는 null일 수 없습니다.")
+        @PathVariable(value = "parentId")
+        @NotEmpty(message = "parentId는 null이거나 값이 없을 수 없습니다.")
         parentId: Long?
     ) {
         createPickleReplyCommentUseCase.createPickleReplyComment(req.toRequest(), pickleId!!, parentId!!)
