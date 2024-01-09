475a70ac425ac037bfb789bcabe0f925c7c94457
HyunSu1768 <azxcv1768@gmail.com>
2024-01-10 08:33:30 +0900

    ♻️ :: ktlint

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleCommentController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleCommentController.kt
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
-import javax.validation.constraints.NotNull
 
 @Tag(name = "Pickle Comment API")
 @Validated
