fdf59686500419a0b7669843954726d06de4c36c
HyunSu1768 <azxcv1768@gmail.com>
2024-01-10 08:21:37 +0900

    ♻️ :: comments

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/dto/PickleCommentWebRequest.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/dto/PickleCommentWebRequest.kt
@@ -1,16 +1,18 @@
 package com.info.maeumgagym.controller.pickle.dto
 
 import com.info.maeumgagym.pickle.dto.request.PickleCommentRequest
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class PickleCommentWebRequest(
     @field:NotNull(message = "null일 수 없습니다")
     @field:Size(min = 1, max = 1000, message = "1자 이상 1000자 이하여야 합니다.")
-    val content: String
+    @field:NotBlank(message = "content는 Null일 수 없습니다.")
+    val content: String?
 ) {
 
     fun toRequest() = PickleCommentRequest(
-        content = content
+        content = content!!
     )
 }
