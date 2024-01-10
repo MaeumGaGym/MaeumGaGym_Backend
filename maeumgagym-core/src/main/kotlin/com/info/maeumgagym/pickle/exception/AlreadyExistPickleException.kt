7b19a34a3374dbfa1b32c31c15125be1736b6eba
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-10 14:27:06 +0900

    🌟 :: 피클 id 중복시 예외 발생

---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pickle/exception/AlreadyExistPickleException.kt
@@ -0,0 +1,6 @@
+package com.info.maeumgagym.pickle.exception
+
+import com.info.maeumgagym.common.exception.ErrorCode
+import com.info.maeumgagym.common.exception.MaeumGaGymException
+
+object AlreadyExistPickleException : MaeumGaGymException(ErrorCode.ALREADY_EXIST_PICKLE)
