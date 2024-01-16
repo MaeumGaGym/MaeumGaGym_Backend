32c81c56e26a44f28bd2b61188479f9b964617fd
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-01-16 12:44:47 +0900

    🔥 :: unused dto

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/wakatime/dto/AddWakaTimeWebRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.info.maeumgagym.controller.wakatime.dto
-
-import javax.validation.constraints.NotNull
-
-data class AddWakaTimeWebRequest(
-
-    @field:NotNull(message = "seconds가 null일 수 없습니다.")
-    val seconds: Long?
-)
