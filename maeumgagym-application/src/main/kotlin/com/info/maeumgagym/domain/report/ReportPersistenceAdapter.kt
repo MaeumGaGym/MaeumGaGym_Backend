fac0aa79f4f6889ce4bbc32f564d544f6e7a898c
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-16 23:17:23 +0900

    ✨ :: Save 구현

---
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/report/ReportPersistenceAdapter.kt
@@ -0,0 +1,19 @@
+package com.info.maeumgagym.domain.report
+
+import com.info.common.PersistenceAdapter
+import com.info.maeumgagym.domain.report.mapper.ReportMapper
+import com.info.maeumgagym.domain.report.repository.ReportRepository
+import com.info.maeumgagym.report.model.Report
+import com.info.maeumgagym.report.port.out.SaveReportPort
+
+@PersistenceAdapter
+internal class ReportPersistenceAdapter(
+    private val reportRepository: ReportRepository,
+    private val reportMapper: ReportMapper
+) : SaveReportPort {
+
+    override fun saveReport(report: Report): Report =
+        reportMapper.toDomain(
+            reportRepository.save(reportMapper.toEntity(report))
+        )
+}
