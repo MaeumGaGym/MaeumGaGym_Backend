b988cb07b9ad6af4de6dd9177812d5f52ba882f8
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-16 23:32:03 +0900

    ✨ :: Exceptions

---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/report/exception/CannotReportOneselfException.kt
@@ -0,0 +1,6 @@
+package com.info.maeumgagym.report.exception
+
+import com.info.maeumgagym.common.exception.ErrorCode
+import com.info.maeumgagym.common.exception.MaeumGaGymException
+
+object CannotReportOneselfException : MaeumGaGymException(ErrorCode.CANNNOT_REPORT_ONESELF)
