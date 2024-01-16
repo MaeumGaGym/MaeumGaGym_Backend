cd334786a3720ee11f0f4f63387c2200e68731ae
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-01-16 12:45:36 +0900

    📝 :: var -> val

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/user/model/User.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/user/model/User.kt
@@ -5,10 +5,10 @@ import java.util.*
 
 data class User(
     val id: UUID = UUID(0, 0),
-    var nickname: String,
+    val nickname: String,
     val roles: MutableList<Role> = mutableListOf(),
     val oauthId: String,
-    var profileImage: String? = null,
-    var wakaStartedAt: LocalDateTime? = null,
-    var isDeleted: Boolean = false
+    val profileImage: String? = null,
+    val wakaStartedAt: LocalDateTime? = null,
+    val isDeleted: Boolean = false
 )
