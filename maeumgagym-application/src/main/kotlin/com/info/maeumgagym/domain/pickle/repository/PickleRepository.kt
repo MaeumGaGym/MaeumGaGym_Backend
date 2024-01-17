8e2e378616152c9a53f4b720801199e297cfa2ba
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-01-17 10:35:35 +0900

    📝 :: ktlint

--- maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/pickle/repository/PickleRepository.kt
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/pickle/repository/PickleRepository.kt
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface PickleRepository : JpaRepository<PickleJpaEntity, String> {
-    fun existsByVideoId(videoId: String): Boolean
 }
 
