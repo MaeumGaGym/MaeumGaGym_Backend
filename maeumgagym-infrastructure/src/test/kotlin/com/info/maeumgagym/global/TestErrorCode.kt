b693d4b5d9f13210a9f71d3a026038a8a3f1f319
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-18 18:51:35 +0900

    ♻ :: 에러 메세지 수정

--- maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/global/TestErrorCode.kt
+++ maeumgagym-infrastructure/src/test/kotlin/com/info/maeumgagym/global/TestErrorCode.kt
@@ -10,5 +10,5 @@ enum class TestErrorCode(
     ERROR_TOO_BIG("발생한 오차가 허용된 오차보다 큽니다."),
 
     // Quote
-    MISMATCH_QUOTE_AND_QUOTER("명언과 화자가 어울리지 않습니다.")
+    MISMATCH_QUOTE_AND_QUOTER("명언과 화자가 설정된 것과 일치하지 않습니다.")
 }
