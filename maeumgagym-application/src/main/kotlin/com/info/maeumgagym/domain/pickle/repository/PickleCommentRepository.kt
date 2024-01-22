bd8d236fc03bce6321499e71fe8d2030d1c7faae
HyunSu1768 <azxcv1768@gmail.com>
2024-01-22 20:50:16 +0900

    ✨ :: PickleCommentRepository Paging

--- maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/pickle/repository/PickleCommentRepository.kt
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/pickle/repository/PickleCommentRepository.kt
@@ -1,10 +1,12 @@
 package com.info.maeumgagym.domain.pickle.repository
 
 import com.info.maeumgagym.domain.pickle.entity.PickleCommentJpaEntity
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface PickleCommentRepository : JpaRepository<PickleCommentJpaEntity, Long> {
-    fun findAllByVideoId(videoId: String): List<PickleCommentJpaEntity>
+    fun findAllByVideoId(videoId: String, pageable: Pageable): Page<PickleCommentJpaEntity>
 }
