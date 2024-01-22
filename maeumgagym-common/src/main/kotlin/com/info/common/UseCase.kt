9e08cdfc5eb3445d2b2a69ec7d19ddd0aab5c641
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-01-22 10:11:09 +0900

    📝 :: lint

--- maeumgagym-common/src/main/kotlin/com/info/common/UseCase.kt
+++ maeumgagym-common/src/main/kotlin/com/info/common/UseCase.kt
@@ -1,7 +1,6 @@
 package com.info.common
 
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
