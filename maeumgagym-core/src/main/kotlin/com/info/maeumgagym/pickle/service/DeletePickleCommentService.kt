c75bf85be14f26134ab14e15966808ab25a3156d
HyunSu1768 <azxcv1768@gmail.com>
2024-01-24 16:14:41 +0900

    ✨ :: DeletePickleCommentService

---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pickle/service/DeletePickleCommentService.kt
@@ -0,0 +1,28 @@
+package com.info.maeumgagym.pickle.service
+
+import com.info.common.UseCase
+import com.info.maeumgagym.auth.exception.PermissionDeniedException
+import com.info.maeumgagym.auth.port.out.ReadCurrentUserPort
+import com.info.maeumgagym.pickle.exception.PickleCommentNotFoundException
+import com.info.maeumgagym.pickle.port.`in`.DeletePickleCommentUseCase
+import com.info.maeumgagym.pickle.port.out.DeletePickleCommentPort
+import com.info.maeumgagym.pickle.port.out.ReadPickleCommentPort
+
+@UseCase
+internal class DeletePickleCommentService(
+    private val deletePickleCommentPort: DeletePickleCommentPort,
+    private val currentUserPort: ReadCurrentUserPort,
+    private val readPickleCommentPort: ReadPickleCommentPort
+): DeletePickleCommentUseCase {
+    override fun deletePickleById(pickleCommentId: Long) {
+        val user = currentUserPort.readCurrentUser()
+        val pickleComment = readPickleCommentPort.readPickleComment(pickleCommentId)
+            ?: throw PickleCommentNotFoundException
+
+        if(pickleComment.writer.oauthId != user.oauthId) {
+            throw PermissionDeniedException
+        }
+
+        deletePickleCommentPort.deletePickleComment(pickleComment)
+    }
+}
