850868fe4d870fa3cc133b0cf3f5fbb98cfc085c
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-01-26 17:17:57 +0900

    🔥 :: 중복 Annotation

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/dto/PickleCommentWebRequest.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/dto/PickleCommentWebRequest.kt
@@ -6,7 +6,6 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class PickleCommentWebRequest(
-    @field:NotNull(message = "null일 수 없습니다")
     @field:Size(min = 1, max = 1000, message = "1자 이상 1000자 이하여야 합니다.")
     @field:NotBlank(message = "content는 Null일 수 없습니다.")
     val content: String?
