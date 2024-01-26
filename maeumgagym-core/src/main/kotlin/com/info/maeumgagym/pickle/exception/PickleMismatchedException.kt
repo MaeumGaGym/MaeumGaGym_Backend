b9cad48dfc273da6340d8c79897f691f502fc391
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-01-26 17:59:23 +0900

    📝 :: rename

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pickle/exception/PickleMismatchedException.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pickle/exception/PickleMismatchedException.kt
@@ -3,4 +3,4 @@ package com.info.maeumgagym.pickle.exception
 import com.info.maeumgagym.common.exception.ErrorCode
 import com.info.maeumgagym.common.exception.MaeumGaGymException
 
-object PickleMismatchException : MaeumGaGymException(ErrorCode.PICKLE_MISMATCHED)
+object PickleMismatchedException : MaeumGaGymException(ErrorCode.PICKLE_MISMATCHED)
