5a0f73aa4a916f6bf1ed1fe974737fb033ab73ba
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-01-26 17:19:19 +0900

    📝 :: Validation 추가

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleCommentController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pickle/PickleCommentController.kt
@@ -8,6 +8,7 @@ import com.info.maeumgagym.pickle.port.`in`.DeletePickleCommentUseCase
 import com.info.maeumgagym.pickle.port.`in`.ReadAllPagedPickleCommentUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,9 +17,13 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
+import javax.validation.constraints.Positive
+import javax.validation.constraints.PositiveOrZero
 
 @Tag(name = "Pickle Comment API")
 @Validated
@@ -31,6 +36,7 @@ class PickleCommentController(
 ) {
     @Operation(summary = "피클 댓글 추가 API")
     @PostMapping("/{videoId}")
+    @ResponseStatus(HttpStatus.CREATED)
     fun createPickleComment(
         @RequestBody @Valid
         req: PickleCommentWebRequest,
@@ -51,17 +57,26 @@ class PickleCommentController(
         @Pattern(regexp = "^[0-9a-f]{8}$")
         @Valid
         videoId: String?,
-        @RequestParam(required = false, defaultValue = "0", value = "page") page: Int,
-        @RequestParam(required = false, defaultValue = "5", value = "size") size: Int
+        @RequestParam(required = false, defaultValue = "0", value = "page")
+        @Valid
+        @PositiveOrZero(message = "0보다 크거나 같아야 합니다.")
+        page: Int,
+        @RequestParam(required = false, defaultValue = "5", value = "size")
+        @Valid
+        @Positive(message = "0보다 커야 합니다.")
+        size: Int
     ): PickleCommentListResponse =
         readAllPagedPickleCommentUseCase.readAllPagedPickleCommentByVideoId(videoId!!, page, size)
 
     @Operation(summary = "피클 댓글 삭제 API")
     @DeleteMapping("/{commentId}")
     fun deletePickleComment(
-        @PathVariable(value = "commentId")
-        commentId: Long
+        @PathVariable(value = "commentId", required = false)
+        @Valid
+        @NotNull(message = "null일 수 없습니다.")
+        @Positive(message = "0보다 커야 합니다.")
+        commentId: Long?
     ) {
-        deletePickleCommentUseCase.deleteFromId(commentId)
+        deletePickleCommentUseCase.deleteFromId(commentId!!)
     }
 }
