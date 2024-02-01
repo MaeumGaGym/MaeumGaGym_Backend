a48f08e9eb79e9e6893dbdc0ddab75a5676ac49d
gurdl7011 <gurdl7011@dsm.hs.kr>
2024-02-01 09:09:11 +0900

    📝 :: Swagger Response Header설정 추가

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/public/PublicController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/public/PublicController.kt
@@ -2,6 +2,9 @@ package com.info.maeumgagym.controller.public
 
 import com.info.common.WebAdapter
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.headers.Header
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
@@ -16,6 +19,16 @@ import org.springframework.web.bind.annotation.ResponseStatus
 class PublicController {
 
     @Operation(summary = "CSRF Token 발급 받기")
+    @ApiResponse(
+        responseCode = "200",
+        headers = [
+            Header(
+                name = "Set-Cookie",
+                description = "CSRF Token",
+                schema = Schema(type = "string", example = "XSRF-TOKEN=...; Secure; HttpOnly; SameSite=Strict")
+            )
+        ]
+    )
     @GetMapping("/csrf")
     @ResponseStatus(HttpStatus.OK)
     fun getCSRFToken() {
