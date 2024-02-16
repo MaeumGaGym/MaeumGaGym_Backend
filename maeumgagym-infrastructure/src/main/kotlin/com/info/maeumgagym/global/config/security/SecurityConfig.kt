bd650116b7d7e74a64a56598a95b25f7f52dcc80
kanghyuk <gi501476@gmail.com>
2024-02-16 09:40:11 +0900

    📝 :: uncomment

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/security/SecurityConfig.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/security/SecurityConfig.kt
@@ -24,7 +24,7 @@ class SecurityConfig(
             .formLogin().disable() // Html Form 로그인 비활성화
             .csrf().csrfTokenRepository(getCsrfTokenRepository()).and() // CSRF 설정
             .cors().and() // CORS 활성화
-            //.requiresChannel().anyRequest().requiresSecure().and() // XSS Attack (HTTPS 요청 요구) local test시 주석 처리할 것
+            .requiresChannel().anyRequest().requiresSecure().and() // XSS Attack (HTTPS 요청 요구) local test시 주석 처리할 것
 //
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
