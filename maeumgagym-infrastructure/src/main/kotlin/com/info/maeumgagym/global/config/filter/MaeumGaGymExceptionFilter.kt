247742217b81d2094ee84dcadce96b560d1afe9e
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-02-19 21:04:18 +0900

    ✨ :: LocalDateConvertor Exception catch 추가

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/filter/MaeumGaGymExceptionFilter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/filter/MaeumGaGymExceptionFilter.kt
@@ -5,11 +5,13 @@ import com.info.maeumgagym.common.exception.ErrorCode
 import com.info.maeumgagym.common.exception.MaeumGaGymException
 import com.info.maeumgagym.global.error.ErrorResponse
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
 import java.nio.charset.StandardCharsets
+import java.time.DateTimeException
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -29,6 +31,14 @@ class MaeumGaGymExceptionFilter(
             filterChain.doFilter(request, response)
         } catch (e: MaeumGaGymException) {
             writerErrorCode(response, e.errorCode)
+        } catch (e: DateTimeException) {
+            response.apply {
+                status = HttpStatus.BAD_REQUEST.value()
+                characterEncoding = StandardCharsets.UTF_8.name()
+                contentType = MediaType.APPLICATION_JSON_VALUE
+                writer.write(objectMapper.writeValueAsString(ErrorResponse(status, e.message)))
+                writer.flush()
+            }
         } catch (e: Exception) {
             e.printStackTrace()
             writerErrorCode(response, ErrorCode.INTERNAL_SERVER_ERROR)
