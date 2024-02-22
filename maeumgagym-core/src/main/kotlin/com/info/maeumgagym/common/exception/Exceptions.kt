69a7fc8f9b0037e4e93347d287a516ed8d4e840e
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-02-22 11:08:53 +0900

    ♻ :: Exception 및 Exception 관련 Filter 개편

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/common/exception/Exceptions.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/common/exception/Exceptions.kt
@@ -6,7 +6,39 @@ import com.info.maeumgagym.common.enum.DomainNames
 /**
  * 마음가짐 내에서 전역적으로 쓰이는 예외 클래스의 최상위 타입
  *
- * 하위 클래스 사용을 권고
+ * 이 타입으로 예외를 발생시키기보단, 하위 클래스 사용을 권고
+ *
+ * | Daybreak312
+ *
+ * 이전의 Exception 사용 방식은 다음과 같았습니다;
+ *
+ * 1. 한 최상위 abstract class(MaeumGaGymException)와,
+ * 2. 하위 object class,
+ * 3. 그 object class의 본질인 Status Code와 Message가 담긴 enum 클래스.
+ *
+ * 이 세 개만을 이용해 예외를 마치 enum처럼 처리했습니다.
+ * ```
+ * abstract class MaeumGaGymException(
+ *     val errorCode: ErrorCode
+ * ) : RuntimeException()
+ *
+ * enum class ErrorCode(
+ *     val status: Int,
+ *     val message: String
+ * ) {
+ *     PICKLE_NOT_FOUND(404, "Pickle Not Found");
+ * }
+ *
+ * object PickleNotFoundException : MaeumGaGymException(ErrorCode.PICKLE_NOT_FOUND)
+ * ```
+ * 하지만 이러한 방식은 하나의 예외를 만들 때마다 ErrorCode를 추가하고, object를 추가하고, 이것을 throw하는 것까지 불필요하게 복잡한 방식으로 만들어야 하는 것에 불편을 느꼈습니다.
+ *
+ * 그래서 저는,
+ * 1. 예외를 발생시키는 자리에서 입력 가능한 [message]로 보다 편하게 예외를 발생시킬 수 있게 하고,
+ * 2. 최상위 타입 [MaeumGaGymException]의 *static field*로 이전의 싱글톤 예외 방식의 이점을 확보했으며,
+ * 3. 다형성을 이용한 Exception 발생 위치 기록의 편리성을 챙겼습니다.
+ *
+ * | Daybreak312
  *
  * @param status Http 표준 상태 코드
  * @param message 해당 예외 발생의 설명
@@ -25,54 +57,54 @@ open class MaeumGaGymException(
     companion object {
 
         // Feign
-        val FEIGN_BAD_REQUEST = MaeumGaGymException(400, "Feign Bad Request")
-        val FEIGN_UNAUTHORIZED = MaeumGaGymException(401, "Feign UnAuthorized")
-        val FEIGN_FORBIDDEN = MaeumGaGymException(403, "Feign Forbidden")
-        val FEIGN_SERVER_ERROR = MaeumGaGymException(500, "Feign Server Error")
+        val FEIGN_BAD_REQUEST = DomainException(400, "Feign Bad Request")
+        val FEIGN_UNAUTHORIZED = DomainException(401, "Feign UnAuthorized")
+        val FEIGN_FORBIDDEN = DomainException(403, "Feign Forbidden")
+        val FEIGN_SERVER_ERROR = DomainException(500, "Feign Server Error")
 
         // No Content
-        val THERE_NO_PICKLES = MaeumGaGymException(204, "업로드된 피클이 존재하지 않습니다.")
+        val THERE_NO_PICKLES = DomainException(204, "업로드된 피클이 존재하지 않습니다.")
 
         // Internal Server Error
         val INTERNAL_SERVER_ERROR = MaeumGaGymException(500, "Internal Server Error")
-        val ROLE_CONVERTER_ERROR = MaeumGaGymException(500, "Role Converter Error")
+        val ROLE_CONVERTER_ERROR = CriticalException(500, "Role Converter Error")
 
         // UnAuthorized
-        val INVALID_TOKEN = MaeumGaGymException(401, "Invalid Token")
-        val EXPIRED_TOKEN = MaeumGaGymException(401, "Expired Token")
-        val UNAUTHORIZED = MaeumGaGymException(401, "Un Authorized")
+        val INVALID_TOKEN = SecurityException(401, "Invalid Token")
+        val EXPIRED_TOKEN = SecurityException(401, "Expired Token")
+        val UNAUTHORIZED = SecurityException(401, "Un Authorized")
 
         // Forbidden
-        val PERMISSION_DENIED = MaeumGaGymException(403, "Permission Denied")
-        val VIDEO_AND_UPLOADER_MISMATCHED = MaeumGaGymException(403, "Video And Uploader Mismatched")
+        val PERMISSION_DENIED = DomainException(403, "Permission Denied")
+        val VIDEO_AND_UPLOADER_MISMATCHED = DomainException(403, "Video And Uploader Mismatched")
 
         // Not Found
-        val USER_NOT_FOUND = MaeumGaGymException(404, "User Not Found")
-        val POSE_NOT_FOUND = MaeumGaGymException(404, "Pose Not Found")
-        val ROUTINE_NOT_FOUND = MaeumGaGymException(404, "Routine Not Found")
-        val PICKLE_NOT_FOUND = MaeumGaGymException(404, "Pickle Not Found")
-        val PICKLE_COMMENT_NOT_FOUND = MaeumGaGymException(404, "Pickle Comment Not Found")
-        val PICKLE_REPLY_NOT_FOUND = MaeumGaGymException(404, "Pickle Reply Not Found")
-        val STEP_NOT_FOUND = MaeumGaGymException(404, "Step Not Found")
+        val USER_NOT_FOUND = DomainException(404, "User Not Found")
+        val POSE_NOT_FOUND = DomainException(404, "Pose Not Found")
+        val ROUTINE_NOT_FOUND = DomainException(404, "Routine Not Found")
+        val PICKLE_NOT_FOUND = DomainException(404, "Pickle Not Found")
+        val PICKLE_COMMENT_NOT_FOUND = DomainException(404, "Pickle Comment Not Found")
+        val PICKLE_REPLY_NOT_FOUND = DomainException(404, "Pickle Reply Not Found")
+        val STEP_NOT_FOUND = DomainException(404, "Step Not Found")
 
         // Bad Request
-        val FILE_TYPE_MISMATCHED = MaeumGaGymException(400, "File Type Mismatched")
-        val EXERCISE_LIST_CANNOT_EMPTY = MaeumGaGymException(400, "Exercise list cannot empty")
-        val PICKLE_MISMATCHED = MaeumGaGymException(400, "Pickle Mismatched")
-        val WAKA_STARTED_NOT_YET = MaeumGaGymException(400, "Wakatime Started Not Yet")
-        val CANNNOT_REPORT_ONESELF = MaeumGaGymException(400, "Cannot Report Oneself")
-        val NOT_UPLOADED_TO_VIDEO_SERVER = MaeumGaGymException(400, "Does Not Uploaded In Video Server")
-        val TAG_TOO_LONG = MaeumGaGymException(400, "Tag Too Long, Tag Cannot Longer than 10")
-        val START_DATE_CANNOT_AFTER_THAN_END_TIME = MaeumGaGymException(400, "Start Date Cannot After than End Time.")
+        val FILE_TYPE_MISMATCHED = DomainException(400, "File Type Mismatched")
+        val EXERCISE_LIST_CANNOT_EMPTY = DomainException(400, "Exercise list cannot empty")
+        val PICKLE_MISMATCHED = DomainException(400, "Pickle Mismatched")
+        val WAKA_STARTED_NOT_YET = DomainException(400, "Wakatime Started Not Yet")
+        val CANNNOT_REPORT_ONESELF = DomainException(400, "Cannot Report Oneself")
+        val NOT_UPLOADED_TO_VIDEO_SERVER = DomainException(400, "Does Not Uploaded In Video Server")
+        val TAG_TOO_LONG = DomainException(400, "Tag Too Long, Tag Cannot Longer than 10")
+        val START_DATE_CANNOT_AFTER_THAN_END_TIME = DomainException(400, "Start Date Cannot After than End Time.")
 
         // Conflict
-        val DUPLICATED_NICKNAME = MaeumGaGymException(409, "Duplicated Nickname")
-        val ALREADY_EXIST_USER = MaeumGaGymException(409, "Already Exists User")
-        val ALREADY_EXIST_PICKLE = MaeumGaGymException(409, "Already Exists Pickle")
-        val ALREADY_EXIST_STEP = MaeumGaGymException(409, "Alreay Exists Step")
-        val ALREADY_STARTED_WAKA = MaeumGaGymException(409, "Already Started Wakatime")
+        val DUPLICATED_NICKNAME = DomainException(409, "Duplicated Nickname")
+        val ALREADY_EXIST_USER = DomainException(409, "Already Exists User")
+        val ALREADY_EXIST_PICKLE = DomainException(409, "Already Exists Pickle")
+        val ALREADY_EXIST_STEP = DomainException(409, "Alreay Exists Step")
+        val ALREADY_STARTED_WAKA = DomainException(409, "Already Started Wakatime")
         val OTHER_ROUTINE_ALREADY_USING_AT_DAY_OF_WEEK =
-            MaeumGaGymException(409, "Other Routine Already Using At Day of week")
+            DomainException(409, "Other Routine Already Using At Day of week")
     }
 }
 
---
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/error/filter/ErrorLogResponseFilter.kt
@@ -0,0 +1,167 @@
+package com.info.maeumgagym.global.error.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.info.maeumgagym.common.exception.*
+import com.info.maeumgagym.global.error.log.ErrorLog
+import com.info.maeumgagym.global.error.log.ErrorLogLayer
+import com.info.maeumgagym.global.error.log.manager.ErrorLogManager
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.servlet.DispatcherServlet
+import org.springframework.web.util.NestedServletException
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.time.LocalDateTime
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * [Exception]이 발생했을 때, [ErrorLog]를 저장하고, [ErrorResponse]를 작성
+ *
+ * [doFilter], 정확히는 [doFilterInternal]를 *try*문으로 감싸 실행.
+ * 이후 발생한 모든 예외를 *catch*해 각 예외에 따라 [ErrorLog] 저장 및 [ErrorResponse]를 작성.
+ *
+ * 원래 [DispatcherServlet] 통과 이후 발생한 예외는 [NestedServletException.cause]로 감싸져 *throw*되지만, [ExceptionConvertFilter]에서 이를 [MaeumGaGymException]의 하위 타입으로 변환함. 자세한 것은 [ExceptionConvertFilter] 참조.
+ *
+ * @see ExceptionConvertFilter
+ */
+@Component
+@Order(0)
+class ErrorLogResponseFilter(
+    private val objectMapper: ObjectMapper,
+    private val errorLogManager: ErrorLogManager
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            filterChain.doFilter(request, response)
+        } catch (e: MaeumGaGymException) {
+            val errorLog = saveErrorLogAndReturn(e, ErrorLogLayer.Companion.of(e))
+
+            if (errorLog.layer == ErrorLogLayer.UNKNOWN) {
+                e.printStackTrace()
+            }
+
+            if (errorLog.layer == ErrorLogLayer.PRESENTATION) {
+                writeFieldErrorResponse(response, errorLog, e as PresentationValidationException)
+            } else {
+                writeCommonErrorResponse(response, errorLog)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            val errorLog = saveErrorLogAndReturn(e)
+            writeCommonErrorResponse(response, errorLog)
+        }
+    }
+
+    private fun saveErrorLogAndReturn(e: Exception, layer: ErrorLogLayer = ErrorLogLayer.UNKNOWN): ErrorLog {
+        when (e) {
+            is PresentationValidationException ->
+                ErrorLog(
+                    status = e.status,
+                    message = e.message + e.fields,
+                    log = e.stackTrace.toString(),
+                    layer = layer
+                )
+
+            is MaeumGaGymException ->
+                ErrorLog(
+                    status = e.status,
+                    message = e.message,
+                    log = e.stackTrace.toString(),
+                    layer = layer
+                )
+
+            else ->
+                ErrorLog(
+                    message = e.message,
+                    log = e.stackTrace.toString(),
+                    layer = layer
+                )
+        }.run {
+            errorLogManager.save(this)
+            return this
+        }
+    }
+
+    /**
+     * 일반적인 예외에 대한 [ErrorResponse]를 작성
+     *
+     * 해당하는 예외들
+     * - [DomainException]
+     * - [FilterException]
+     * - [InterceptorException]
+     * - [SecurityException]
+     * - [CriticalException]
+     * - 그 외 [PresentationValidationException]을 제외한 나머지 [Exception]
+     */
+    @Throws(IOException::class)
+    private fun writeCommonErrorResponse(response: HttpServletResponse, errorLog: ErrorLog) {
+        response.errorResponseDefaultSetting(errorLog)
+        response.writeErrorResponse(errorLog.run {
+            ErrorResponse(status, message, id, timestamp)
+        })
+    }
+
+    /**
+     * Presentation에서 Validation이 실패했을 떄의 예외에 대한 [ErrorResponse]를 작성
+     *
+     * 해당하는 예외
+     * - [PresentationValidationException]
+     */
+    @Throws(IOException::class)
+    private fun writeFieldErrorResponse(
+        response: HttpServletResponse,
+        errorLog: ErrorLog,
+        e: PresentationValidationException
+    ) {
+        response.errorResponseDefaultSetting(errorLog)
+        response.writeErrorResponse(errorLog.run {
+            ErrorResponse(status, message, id, timestamp, e.fields)
+        })
+    }
+
+    /**
+     * [ErrorResponse]를 작성할 때의 기본 정보들에 대한 설정
+     */
+    private fun HttpServletResponse.errorResponseDefaultSetting(errorLog: ErrorLog) {
+        status = errorLog.status
+        characterEncoding = StandardCharsets.UTF_8.name()
+        contentType = MediaType.APPLICATION_JSON_VALUE
+    }
+
+    /**
+     * [ErrorResponse]의 실제 작성 부분
+     */
+    private fun HttpServletResponse.writeErrorResponse(errorResponse: ErrorResponse) {
+        writer.write(
+            objectMapper.writeValueAsString(errorResponse)
+        )
+        writer.flush()
+    }
+
+    /**
+     * Error의 정보를 담고 있는 Response
+     *
+     * @param status 상태 코드.
+     * @param message 예외 메세지.
+     * @param errorLogId 저장된 에러 로그의 Key.
+     * @param timestamp timestamp
+     * @param map Field Error 등 경우에 따라 Key-Value의 정보가 추가적으로 필요할 경우 사용
+     *  @see writeErrorResponse
+     */
+    data class ErrorResponse(
+        val status: Int,
+        val message: String?,
+        val errorLogId: String,
+        val timestamp: LocalDateTime = LocalDateTime.now(),
+        val map: Map<String, String> = mapOf()
+    )
+}
