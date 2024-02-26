1e579422e633d5203fed47f54fa3b6344c7ad290
HyunSu1768 <azxcv1768@gmail.com>
2024-02-26 17:59:51 +0900

    ♻️ :: Log

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/security/SecurityConfig.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/security/SecurityConfig.kt
@@ -6,10 +6,12 @@ import com.info.maeumgagym.global.error.CustomAuthenticationEntryPoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 
+@EnableWebSecurity(debug = true)
 @Configuration
 class SecurityConfig(
     private val csrfProperties: CSRFProperties,
