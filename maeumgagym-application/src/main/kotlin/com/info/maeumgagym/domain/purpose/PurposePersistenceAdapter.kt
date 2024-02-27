4fd8fbc4c879fb63f476fd8cd70ca06d0ec82f88
kanghyuk <gi501476@gmail.com>
2024-02-28 03:35:45 +0900

    📝 ::

--- maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/purpose/PurposePersistenceAdapter.kt
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/purpose/PurposePersistenceAdapter.kt
@@ -4,16 +4,21 @@ import com.info.common.PersistenceAdapter
 import com.info.maeumgagym.domain.purpose.mapper.PurposeMapper
 import com.info.maeumgagym.domain.purpose.repository.PurposeRepository
 import com.info.maeumgagym.purpose.model.Purpose
+import com.info.maeumgagym.purpose.port.out.DeletePurposePort
 import com.info.maeumgagym.purpose.port.out.ReadPurposePort
 import com.info.maeumgagym.purpose.port.out.SavePurposePort
+import org.springframework.transaction.annotation.Isolation
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional(isolation = Isolation.REPEATABLE_READ, rollbackFor = [Exception::class])
 @PersistenceAdapter
 internal class PurposePersistenceAdapter(
     private val purposeRepository: PurposeRepository,
     private val purposeMapper: PurposeMapper
-) : SavePurposePort,
-    ReadPurposePort {
+) : SavePurposePort, ReadPurposePort, DeletePurposePort {
 
+    @Transactional(propagation = Propagation.MANDATORY)
     override fun save(purpose: Purpose): Purpose =
         purposeMapper.toDomain(
             purposeRepository.save(purposeMapper.toEntity(purpose))
@@ -23,4 +28,9 @@ internal class PurposePersistenceAdapter(
         purposeRepository.findById(id)?.let {
             purposeMapper.toDomain(it)
         }
+
+    @Transactional(propagation = Propagation.MANDATORY)
+    override fun deleteById(id: Long) {
+        purposeRepository.deleteById(id)
+    }
 }
