896bcd194b669e9cfa7605c14fb23a23397a7540
kanghyuk <gi501476@gmail.com>
2024-03-06 11:25:24 +0900

    📝 :: CSRF Temporary disuse

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/security/SecurityConfig.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/global/config/security/SecurityConfig.kt
@@ -22,7 +22,7 @@ class SecurityConfig(
     protected fun filterChain(http: HttpSecurity): SecurityFilterChain =
         http
             .formLogin().disable() // Html Form 로그인 비활성화
-            .csrf().csrfTokenRepository(getCsrfTokenRepository()).and() // CSRF 설정
+//            .csrf().csrfTokenRepository(getCsrfTokenRepository()).and() // CSRF 설정 (temporary disuse)
             .cors().and() // CORS 활성화
             .requiresChannel().anyRequest().requiresSecure().and() // XSS Attack (HTTPS 요청 요구) local test시 주석 처리할 것
 //
