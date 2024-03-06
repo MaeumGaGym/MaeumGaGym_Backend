e8aebc2b1dae0572e5072965d45444de25673877
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-06 19:07:29 +0900

    📄 :: 문서화 추가

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/step/port/in/UpdateStepUseCase.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/step/port/in/UpdateStepUseCase.kt
@@ -1,5 +1,16 @@
 package com.info.maeumgagym.step.port.`in`
 
+import com.info.maeumgagym.step.model.Step
+
+/**
+ * 금일의 걸음 수 카운팅에 걸음 수를 추가적으로 누적하는 기능
+ *
+ * [CreateStepUseCase]를 통해 걸음 수 카운팅을 시작한 후, [UpdateStepUseCase]를 통해 걸음 수를 누적
+ *
+ * 카운팅을 시작하지 않았을 경우 오류 발생
+ *
+ * 현재 [Step]은 금일의 것만 저장
+ */
 interface UpdateStepUseCase {
     fun updateStep(numberOfSteps: Int)
 }
