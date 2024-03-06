96360a650baf22236884a8104cfef6fe0ba7a7b2
kanghyuk <gi501476@gmail.com>
2024-03-06 09:53:27 +0900

    ✨ :: Daily 삭제 API

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/daily/DailyController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/daily/DailyController.kt
@@ -5,17 +5,16 @@ import com.info.maeumgagym.controller.daily.dto.CreateDailyRequest
 import com.info.maeumgagym.daily.dto.response.DailyListResponse
 import com.info.maeumgagym.daily.dto.response.PreSignedURLResponse
 import com.info.maeumgagym.daily.port.`in`.CreateDailyUseCase
+import com.info.maeumgagym.daily.port.`in`.DeleteDailyUseCase
 import com.info.maeumgagym.daily.port.`in`.GetDailyPreSignedURLUseCase
 import com.info.maeumgagym.daily.port.`in`.ReadDailyUseCase
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
 import javax.validation.Valid
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Positive
 import javax.validation.constraints.PositiveOrZero
 
@@ -25,6 +24,7 @@ import javax.validation.constraints.PositiveOrZero
 class DailyController(
     private val createDailyUseCase: CreateDailyUseCase,
     private val readDailyUseCase: ReadDailyUseCase,
+    private val deleteDailyUseCase: DeleteDailyUseCase,
     private val getDailyPreSignedURLUseCase: GetDailyPreSignedURLUseCase
 ) {
     @GetMapping("/daily/pre-signed")
@@ -47,12 +47,21 @@ class DailyController(
     @GetMapping("/dailies")
     fun dailiesRead(
         @Valid
-        @PositiveOrZero
+        @PositiveOrZero(message = "0보다 크거나 같아야 합니다.")
         @RequestParam("page", required = false, defaultValue = "0")
         page: Int,
         @Valid
-        @Positive
+        @Positive(message = "0보다 커야 합니다.")
         @RequestParam("size", required = false, defaultValue = "15")
         size: Int
     ): DailyListResponse = readDailyUseCase.readDailies(page, size)
+
+    @DeleteMapping("/daily/{date}")
+    fun dailyDelete(
+        @Valid @NotNull(message = "null일 수 없습니다.")
+        @PathVariable("date", required = false)
+        date: LocalDate?
+    ) {
+        deleteDailyUseCase.delete(date!!)
+    }
 }
