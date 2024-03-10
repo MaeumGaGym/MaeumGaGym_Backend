88132dd3d49d13f96e93e166cf5b3a75229cb06e
kanghyuk <gi501476@gmail.com>
2024-03-10 23:41:19 +0900

    🔥 :: unused code

--- maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/routine/repository/RoutineHistoryRepository.kt
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/routine/repository/RoutineHistoryRepository.kt
@@ -1,7 +1,6 @@
 package com.info.maeumgagym.domain.routine.repository
 
 import com.info.maeumgagym.domain.routine.entity.RoutineHistoryJpaEntity
-import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.Repository
 import java.time.LocalDate
 import java.util.*
@@ -12,6 +11,4 @@ interface RoutineHistoryRepository : Repository<RoutineHistoryJpaEntity, Long> {
     fun save(entity: RoutineHistoryJpaEntity): RoutineHistoryJpaEntity
 
     fun findByUserIdAndDate(userId: UUID, date: LocalDate): RoutineHistoryJpaEntity?
-
-    fun findByUserIdAndDate(userId: UUID, date: LocalDate, pageable: Pageable): RoutineHistoryJpaEntity?
 }
