f185a4a6a4ca5e9a48db0b9ed7a46d8365852450
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-11 12:02:32 +0900

    ♻ :: ReadPoseUseCase -> ReadPoseFromIdUseCase

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pose/PoseController.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/pose/PoseController.kt
@@ -2,7 +2,7 @@ package com.info.maeumgagym.controller.pose
 
 import com.info.common.WebAdapter
 import com.info.maeumgagym.pose.dto.response.PoseDetailResponse
-import com.info.maeumgagym.pose.port.`in`.ReadPoseUseCase
+import com.info.maeumgagym.pose.port.`in`.ReadPoseFromIdUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.validation.annotation.Validated
@@ -17,7 +17,7 @@ import javax.validation.constraints.Positive
 @WebAdapter
 @Validated
 class PoseController(
-    private val readPoseUseCase: ReadPoseUseCase
+    private val readPoseFromIdUseCase: ReadPoseFromIdUseCase
 ) {
 
     @Operation(summary = "포즈 조회 API")
@@ -27,5 +27,5 @@ class PoseController(
         @Valid
         @Positive(message = "0보다 커야 합니다.")
         id: Long
-    ): PoseDetailResponse = readPoseUseCase.detailResponseById(id)
+    ): PoseDetailResponse = readPoseFromIdUseCase.detailResponseById(id)
 }
