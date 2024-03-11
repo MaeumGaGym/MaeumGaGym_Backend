7e10d95660147a083715a977b692917c8ccea9a3
kanghyuk <gi501476@gmail.com>
2024-03-11 09:41:26 +0900

    ♻️ :: 범위 지정 함수 -> if else

--- maeumgagym-core/src/main/kotlin/com/info/maeumgagym/routine/service/CompleteRoutineService.kt
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/routine/service/CompleteRoutineService.kt
@@ -5,6 +5,7 @@ import com.info.maeumgagym.auth.port.out.ReadCurrentUserPort
 import com.info.maeumgagym.common.exception.BusinessLogicException
 import com.info.maeumgagym.routine.model.RoutineHistory
 import com.info.maeumgagym.routine.port.`in`.CompleteTodayRoutineUseCase
+import com.info.maeumgagym.routine.port.out.ExistsRoutineHistoryPort
 import com.info.maeumgagym.routine.port.out.ReadRoutineHistoryPort
 import com.info.maeumgagym.routine.port.out.ReadRoutinePort
 import com.info.maeumgagym.routine.port.out.SaveRoutineHistoryPort
@@ -14,8 +15,8 @@ import java.time.LocalDate
 class CompleteRoutineService(
     private val readCurrentUserPort: ReadCurrentUserPort,
     private val readRoutinePort: ReadRoutinePort,
-    private val readRoutineHistoryPort: ReadRoutineHistoryPort,
-    private val saveRoutineHistoryPort: SaveRoutineHistoryPort
+    private val saveRoutineHistoryPort: SaveRoutineHistoryPort,
+    private val existsRoutineHistoryPort: ExistsRoutineHistoryPort
 ) : CompleteTodayRoutineUseCase {
     override fun complete() {
         val user = readCurrentUserPort.readCurrentUser()
@@ -25,9 +26,9 @@ class CompleteRoutineService(
         val routine = readRoutinePort.readByUserIdAndDayOfWeek(user.id!!, now.dayOfWeek)
             ?: throw BusinessLogicException.ROUTINE_NOT_FOUND
 
-        readRoutineHistoryPort.readByUserIdAndDate(user.id, now)?.let {
+        if (existsRoutineHistoryPort.exsitsByUserIdAndDate(user.id, now)) {
             throw BusinessLogicException(409, "Already Completed Routine")
-        } ?: saveRoutineHistoryPort.save(
+        } else saveRoutineHistoryPort.save(
             RoutineHistory(
                 id = null,
                 date = now,
