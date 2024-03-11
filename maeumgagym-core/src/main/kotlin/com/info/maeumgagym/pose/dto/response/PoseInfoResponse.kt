b3bbb4816eca89cf976233d2e552e425855e7c9d
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-11 13:48:03 +0900

    ✨ :: 자세 태그 기반 검색 기능 Core 계층 추가

---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pose/dto/response/PoseInfoResponse.kt
@@ -0,0 +1,10 @@
+package com.info.maeumgagym.pose.dto.response
+
+data class PoseInfoResponse(
+    val id: Long,
+    val needMachine: Boolean,
+    val name: String,
+    val simplePart: List<String>,
+    val exactPart: List<String>,
+    val thumbnail: String,
+)
---
+++ maeumgagym-core/src/main/kotlin/com/info/maeumgagym/pose/dto/response/PoseListResponse.kt
@@ -0,0 +1,5 @@
+package com.info.maeumgagym.pose.dto.response
+
+data class PoseListResponse(
+    val details: List<PoseInfoResponse>
+)
