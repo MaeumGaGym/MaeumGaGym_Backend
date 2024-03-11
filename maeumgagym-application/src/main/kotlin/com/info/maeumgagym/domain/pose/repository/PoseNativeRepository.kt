b064ab69ac5ef8a3383a7414f7ba29efea1c447f
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-11 22:20:48 +0900

    🐛 :: Repository를 사용할 수 없는 버그 수정

--- maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/pose/repository/PoseNativeRepository.kt
+++ maeumgagym-application/src/main/kotlin/com/info/maeumgagym/domain/pose/repository/PoseNativeRepository.kt
@@ -2,18 +2,17 @@ package com.info.maeumgagym.domain.pose.repository
 
 import com.info.maeumgagym.TableNames
 import com.info.maeumgagym.domain.pose.entity.PoseJpaEntity
-import com.info.maeumgagym.domain.routine.entity.RoutineJpaEntity
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.Repository
 import org.springframework.data.repository.query.Param
 
 @org.springframework.stereotype.Repository
-interface PoseNativeRepository : Repository<RoutineJpaEntity, Long?> {
+interface PoseNativeRepository : Repository<PoseJpaEntity, Long?> {
 
     @Query(
         value = "SELECT * FROM ${TableNames.POSE_TABLE} p " +
-            "WHERE p.simple_part LIKE :tag OR p.exact_part LIKE :tag OR " +
-            "p.simple_name LIKE :tag OR p.exact_name LIKE :tag",
+            "WHERE p.simple_part LIKE %:tag% OR p.exact_part LIKE %:tag% OR " +
+            "p.simple_name LIKE %:tag% OR p.exact_name LIKE %:tag%",
         nativeQuery = true
     )
     fun readByTag(@Param("tag") tag: String): List<PoseJpaEntity>
