9653134a8d806ca2874d2d72439e4b5d59871bd7
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-21 01:18:07 +0900

    REFACT :: ktlintFormat

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/ReadCurrentUserAdapter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/ReadCurrentUserAdapter.kt
@@ -23,7 +23,7 @@ internal class ReadCurrentUserAdapter(
                 // null인 경우 User를 Load 및 입력
                 this.authenticatedUser = ThreadLocal.withInitial {
                     readUserPort.readByOAuthId(authentication.principal.toString())
-                    // authDetails에 담긴 username = oauthId는 로직상 무조건 유저가 존재해야하므로 AuthenticationException throw
+                        // authDetails에 담긴 username = oauthId는 로직상 무조건 유저가 존재해야하므로 AuthenticationException throw
                         ?: throw AuthenticationException(401, "User Not Found In ReadCurrentUserPort")
                 }
             }
