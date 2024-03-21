d35d2bc1d8a9127b9026cb9958d2278fc0a03c43
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-21 15:02:50 +0900

    FIX :: ReadCurrentUserAdapter 로직 수정

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/ReadCurrentUserAdapter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/auth/ReadCurrentUserAdapter.kt
@@ -1,7 +1,6 @@
 package com.info.maeumgagym.auth
 
 import com.info.maeumgagym.auth.port.out.ReadCurrentUserPort
-import com.info.maeumgagym.common.exception.CriticalException
 import com.info.maeumgagym.security.jwt.AuthenticationProvider
 import com.info.maeumgagym.security.jwt.JwtFilter
 import com.info.maeumgagym.user.model.User
@@ -21,23 +20,16 @@ internal class ReadCurrentUserAdapter(
 
         JwtFilter.run {
             // Lazy Loading으로 Nullable인 User를 확인
-            if (this.authenticatedUser == null) {
-                // null인 경우 User를 Load 및 입력
-                this.authenticatedUser = ThreadLocal.withInitial {
-                    readUserPort.readByOAuthId(authentication.principal as String)
-                        // authDetails에 담긴 username = oauthId는 로직상 무조건 유저가 존재해야하므로 CriticalException throw
-                        ?: throw CriticalException(401, "User not found in Authentication data At ReadCurrentUserPort")
-                }
+            if (this.authenticatedUser?.get() == null
+                || this.authenticatedUser?.get()?.oauthId != authentication!!.principal
+            ) {
+                // null인 경우 User를 Load 및 SecurityContext, authenticatedUser에 입력
+                SecurityContextHolder.getContext().authentication =
+                    authenticationProvider.getAuthentication(
+                        authentication.principal as String
+                    )
             }
         }
-
-        // Loaded User를 SecurityContext Authentication에도 반영
-        SecurityContextHolder.getContext().authentication =
-            authenticationProvider.getAuthentication(
-                JwtFilter.authenticatedUser?.get()?.oauthId
-                    // User는 위에서 설정해주었으므로 비어있을 수 없음
-                    ?: throw CriticalException(500, "User loaded but It's NULL.")
-            )
 
         // User 반환
         return JwtFilter.authenticatedUser!!.get()
