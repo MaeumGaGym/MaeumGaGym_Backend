a3b35c60d76704a8942dbcb50b6c53fec1275f2a
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-22 14:21:42 +0900

    FIX :: FilterChain 임계 영역을 ThreadLocal로 수정

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/filter/filterchain/ExceptionChainedFilterChain.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/filter/filterchain/ExceptionChainedFilterChain.kt
@@ -8,27 +8,29 @@ class ExceptionChainedFilterChain(
     private val filters: Map<String, Filter>
 ) : ChainedFilterChain() {
 
-    private var calledFilterChain: FilterChain? = null
+    private var calledFilterChain: ThreadLocal<FilterChain>? = null
 
-    private var currentFilterIndex: Int = -1
+    private var currentFilterIndex: ThreadLocal<Int> = ThreadLocal.withInitial {
+        -1
+    }
 
     override fun getFilters(): Map<String, Filter> = filters
 
-    override fun getCalledFilterChain(): FilterChain? = calledFilterChain
+    override fun getCalledFilterChain(): FilterChain? = calledFilterChain?.get()
 
-    override fun getCurrentFilterIndex(): Int = this.currentFilterIndex
+    override fun getCurrentFilterIndex(): Int = this.currentFilterIndex.get()
 
     override fun plusCurrentFilterIndex(): Int {
-        this.currentFilterIndex += 1
-        return this.currentFilterIndex
+        this.currentFilterIndex.set(this.getCurrentFilterIndex() + 1)
+        return this.currentFilterIndex.get()
     }
 
     override fun resetFilterChain() {
         this.calledFilterChain = null
-        this.currentFilterIndex = -1
+        this.currentFilterIndex = ThreadLocal.withInitial { -1 }
     }
 
     override fun setCalledFilterChain(filterChain: FilterChain) {
-        this.calledFilterChain = filterChain
+        this.calledFilterChain = ThreadLocal.withInitial { filterChain }
     }
 }
