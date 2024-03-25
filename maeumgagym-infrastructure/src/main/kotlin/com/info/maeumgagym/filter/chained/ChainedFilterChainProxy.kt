db9fee248d52c916c5b23856431ee6350fed5e16
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-25 12:49:53 +0900

    FIX :: ChainedFilterChainProxy Used Wrong Function

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/filter/chained/ChainedFilterChainProxy.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/filter/chained/ChainedFilterChainProxy.kt
@@ -1,8 +1,15 @@
 package com.info.maeumgagym.filter.chained
 
 import com.info.maeumgagym.filter.global.GlobalFilterChainProxy
+import javax.servlet.FilterChain
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
 
 abstract class ChainedFilterChainProxy<out T : ChainedFilterChain> : GlobalFilterChainProxy<T>() {
 
     abstract override val filterChain: T
+
+    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
+        this.filterChain.doFilterChained(request, response, chain)
+    }
 }
