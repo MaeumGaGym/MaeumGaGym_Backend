c8e932ababa3030a9ee99f58b7ca8198f7f442b3
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-27 22:32:00 +0900

    ANNO :: TokenRepository 문서화

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/jwt/repository/AccessTokenRepository.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/jwt/repository/AccessTokenRepository.kt
@@ -3,6 +3,14 @@ package com.info.maeumgagym.security.jwt.repository
 import com.info.maeumgagym.security.jwt.entity.AccessTokenRedisEntity
 import org.springframework.data.repository.Repository
 
+/**
+ * Access Token을 저장하는 Redis의 DAO Repository
+ *
+ * @see RefreshTokenRepository
+ *
+ * @author gurdl0525
+ * @since 28-12-2023
+ */
 @org.springframework.stereotype.Repository
 interface AccessTokenRepository : Repository<AccessTokenRedisEntity, String> {
 
