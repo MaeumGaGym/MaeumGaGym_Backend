9fe5a8e284eaa745b57d821c67897011580f04e4
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-27 22:52:10 +0900

    ANNO :: 문서화

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/repository/ExceptionRepository.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/repository/ExceptionRepository.kt
@@ -1,5 +1,11 @@
 package com.info.maeumgagym.error.repository
 
+/**
+ * 경우에 따라, 생성된 Exception을 저장해두었다가 다른 위치에서 발생시킬 수 있도록 Thread에 전역적으로 저장하기 위한 인터페이스
+ *
+ * @author Daybreak312
+ * @since 27-03-2024
+ */
 interface ExceptionRepository {
 
     fun save(e: Exception)
--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/repository/ExceptionRepositoryImpl.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/repository/ExceptionRepositoryImpl.kt
@@ -2,6 +2,12 @@ package com.info.maeumgagym.error.repository
 
 import org.springframework.stereotype.Component
 
+/**
+ * Docs는 상위 타입에 존재
+ *
+ * @author Daybreak312
+ * @since 27-03-2024
+ */
 @Component
 private class ExceptionRepositoryImpl : ExceptionRepository {
 
