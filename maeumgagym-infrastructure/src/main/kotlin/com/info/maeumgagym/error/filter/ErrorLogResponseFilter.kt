79c388725101dc1a80aed03804e57c6437507999
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-28 15:49:24 +0900

    ANNO :: 수상한 dependency 수정

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/filter/ErrorLogResponseFilter.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/filter/ErrorLogResponseFilter.kt
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse
  *
  * 원래 [org.springframework.web.servlet.DispatcherServlet] 통과 이후 발생한 예외는 [NestedServletException.cause]로 감싸져 *throw*되지만, [ExceptionConvertFilter]에서 이를 [MaeumGaGymException]의 하위 타입으로 변환함. 자세한 것은 [ExceptionConvertFilter] 참조.
  *
- * 해당 *Filter*의 순서 설정 정보는 [com.info.maeumgagym.config.config.ApplicationFilterChainConfig]에 존재
+ * 해당 *Filter*의 순서 설정 정보는 [com.info.maeumgagym.filter.config.ApplicationFilterChainConfig]에 존재
  *
  * @see ExceptionConvertFilter
  *
