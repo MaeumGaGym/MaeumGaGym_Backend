761d289d319a02b4fbca324260e7240802c387a2
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-03-29 13:49:50 +0900

    REFACT :: 쓰이지 않는 @Throws 제거 및 주석 작성

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/repository/ExceptionRepository.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/error/repository/ExceptionRepository.kt
@@ -1,7 +1,7 @@
 package com.info.maeumgagym.error.repository
 
 /**
- * 경우에 따라, 생성된 Exception을 저장해두었다가 다른 위치에서 발생시킬 수 있도록 Thread에 전역적으로 저장하기 위한 인터페이스
+ * 생성된 Exception을 저장해두었다가 다른 위치에서 이용할 수 있도록 Thread에 전역적으로 저장하기 위한 인터페이스
  *
  * @author Daybreak312
  * @since 27-03-2024
@@ -10,6 +10,5 @@ interface ExceptionRepository {
 
     fun save(e: Exception)
 
-    @Throws(Exception::class)
     fun get(): Exception?
 }
