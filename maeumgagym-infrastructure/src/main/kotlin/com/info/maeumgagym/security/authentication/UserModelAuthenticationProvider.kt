0331ab0807171fb41ef11c775cbfe33015722e3f
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-02 18:41:03 +0900

    ANNO :: 주석 작성

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/authentication/UserModelAuthenticationProvider.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/authentication/UserModelAuthenticationProvider.kt
@@ -4,7 +4,13 @@ import com.info.maeumgagym.security.authentication.vo.UserModelAuthentication
 
 interface UserModelAuthenticationProvider : AuthenticationProvider {
 
+    /**
+     * 생성한 [UserModelAuthentication]의 [UserModelAuthentication.user]를 [subject]를 기반으로 유저를 탐색해 삽입 후 반환
+     */
     override fun getAuthentication(subject: String): UserModelAuthentication
 
+    /**
+     * [UserModelAuthentication.user]가 비어있는 [UserModelAuthentication] 반환
+     */
     fun getEmptyAuthentication(subject: String): UserModelAuthentication
 }
