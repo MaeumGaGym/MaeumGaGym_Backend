5442c8539c239fe0466900a8f2db88ef6b674312
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-06 11:28:55 +0900

    REFACT :: Log Level 수정

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/config/SecurityLogConfig.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/config/SecurityLogConfig.kt
@@ -4,8 +4,9 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.stereotype.Component
 
-//@Component
+@Component
 class SecurityLogConfig {
     @Value("\${spring.websecurity.debug:false}")
     var webSecurityDebug: Boolean = false
