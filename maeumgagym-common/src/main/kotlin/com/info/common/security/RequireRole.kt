dd6922c6ad05bc283156acfa0abcf4bbd17fccca
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-10 16:38:04 +0900

    ANNO :: 주석 수정

--- maeumgagym-common/src/main/kotlin/com/info/common/security/RequireRole.kt
+++ maeumgagym-common/src/main/kotlin/com/info/common/security/RequireRole.kt
@@ -1,10 +1,7 @@
 package com.info.common.security
 
 /**
- * [@WebAdapter][com.info.common.WebAdapter] 어노테이션이 부착된 컨트롤러 단위로, Role 인증이 필요함을 명시하기 위한 어노테이션
- *
- * @param role [Role][com.info.maeumgagym.user.model.Role]의 문자열 이름
- *
+ * [@WebAdapter][com.info.common.responsibility.WebAdapter] 어노테이션이 부착된 컨트롤러 단위로, [Role][com.info.maeumgagym.user.model.Role] 인증이 필요함을 명시하기 위한 어노테이션
  *
  * ```
  * // Use like this:
@@ -32,6 +29,8 @@ package com.info.common.security
  *
  * @RequireRole("USER")
  * ```
+ *
+ * @param role [Role][com.info.maeumgagym.user.model.Role]의 문자열 이름
  *
  * @author Daybreak312
  * @since 10-04-2024
