b481ab8013e25f48af39deb8b3ecb16fd776d3bf
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-10 11:26:58 +0900

    ANNO :: 주석 추가

--- maeumgagym-common/src/main/kotlin/com/info/common/security/NeedRole.kt
+++ maeumgagym-common/src/main/kotlin/com/info/common/security/NeedRole.kt
@@ -1,5 +1,13 @@
 package com.info.common.security
 
+/**
+ * [@WebAdapter][com.info.common.WebAdapter] 어노테이션이 부착된 컨트롤러 단위로, Role 인증이 필요함을 명시하기 위한 어노테이션
+ *
+ * @param role [Role][com.info.maeumgagym.user.model.Role]의 문자열 이름
+ *
+ * @author Daybreak312
+ * @since 10-04-2024
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class NeedRole(
