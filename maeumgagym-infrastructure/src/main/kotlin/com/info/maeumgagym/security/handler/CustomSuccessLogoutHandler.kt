ba077367927aa998d3deb876f28a0b9725f58952
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-11 20:23:04 +0900

    ANNO :: Logout 처리 클래스 Docs 작성

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/handler/CustomSuccessLogoutHandler.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/handler/CustomSuccessLogoutHandler.kt
@@ -6,8 +6,17 @@ import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+/**
+ * [LogoutFilter][org.springframework.security.web.authentication.logout.LogoutFilter]에서, [CustomLogoutHandler]가 Logout에 성공했을 때의 처리를 담당하는 Handler.
+ *
+ * @see CustomLogoutHandler
+ *
+ * @author HyunSu1768
+ * @since 03-11-2024
+ */
 @Component
 class CustomSuccessLogoutHandler : LogoutSuccessHandler {
+
     override fun onLogoutSuccess(
         request: HttpServletRequest,
         response: HttpServletResponse,
