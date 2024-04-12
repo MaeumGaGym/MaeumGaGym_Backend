6aeed6ea776dfe700f458e5a946b9b6054528433
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-12 15:50:13 +0900

    REFACT :: RoutineName 글자 수 제한 추가

--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/dto/CreateRoutineWebRequest.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/dto/CreateRoutineWebRequest.kt
@@ -6,10 +6,12 @@ import com.info.maeumgagym.routine.dto.request.ExerciseInfoRequest
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 class CreateRoutineWebRequest(
 
     @field:NotBlank(message = "null일 수 없습니다")
+    @field:Size(min = 1, max = 12, message = "1글자에서 12글자 사이여야 합니다.")
     val routineName: String?,
 
     @field:NotNull(message = "null일 수 없습니다")
--- maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/dto/UpdateRoutineWebRequest.kt
+++ maeumgagym-presentation/src/main/kotlin/com/info/maeumgagym/controller/routine/dto/UpdateRoutineWebRequest.kt
@@ -6,9 +6,11 @@ import com.info.maeumgagym.routine.dto.request.UpdateRoutineRequest
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 data class UpdateRoutineWebRequest(
     @field:NotBlank(message = "null일 수 없습니다")
+    @field:Size(min = 1, max = 12, message = "1글자에서 12글자 사이여야 합니다.")
     val routineName: String?,
 
     @field:NotNull(message = "null일 수 없습니다")
