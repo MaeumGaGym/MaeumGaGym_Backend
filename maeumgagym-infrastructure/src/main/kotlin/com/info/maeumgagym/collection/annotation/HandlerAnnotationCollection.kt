85996875f95e329b07b1a9e883f0c41d5ca3a68e
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-22 19:40:22 +0900

    ENCHANT :: Method의 어노테이션도 수집 가능하도록 수정

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/collection/annotation/HandlerAnnotationCollection.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/collection/annotation/HandlerAnnotationCollection.kt
@@ -3,6 +3,7 @@ package com.info.maeumgagym.collection.annotation
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
+import java.lang.reflect.Method
 import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotations
 
@@ -14,6 +15,10 @@ class HandlerAnnotationCollection : AnnotationCollection {
             is HandlerMethod -> {
                 return `object`.method.getAnnotation(annotation.java)
                     ?: `object`.bean::class.findAnnotations(annotation).firstOrNull()
+            }
+
+            is Method -> {
+                return `object`.getAnnotation(annotation.java)
             }
 
             is MethodParameter -> {
