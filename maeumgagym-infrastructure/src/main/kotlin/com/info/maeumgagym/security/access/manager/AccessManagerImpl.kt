028c9193caa122d8b14fc37ceb8ff7aa6def3c6a
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-22 19:56:19 +0900

    FIX :: 의도되지 않은 타입 변환 제거

--- maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/access/manager/AccessManagerImpl.kt
+++ maeumgagym-infrastructure/src/main/kotlin/com/info/maeumgagym/security/access/manager/AccessManagerImpl.kt
@@ -10,10 +10,13 @@ class AccessManagerImpl(
 ) : AccessManager {
 
     override fun checkAccessAllowed(handler: Any) {
+        // 매핑된 핸들러, 정확힌 매핑된 메소드를 HandlerMethod 타입으로 캐스팅
         val handlerMethod = castHandlerToHandlerMethodOrNull(handler) ?: return
 
+        // Controller 클래스 자체의 어노테이션 확인
         annotationBasedUserAuthenticationChecker.check(handlerMethod.bean)
-        annotationBasedUserAuthenticationChecker.check(handlerMethod.method)
+        // 매핑된 메소드에 부착된 어노테이션 확인
+        annotationBasedUserAuthenticationChecker.check(handlerMethod)
     }
 
     private fun castHandlerToHandlerMethodOrNull(handler: Any): HandlerMethod? {
