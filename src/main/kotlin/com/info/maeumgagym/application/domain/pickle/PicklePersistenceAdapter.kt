ff95c0fd33db159b58be48a63a1dc4c7ba24d837
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-04-25 18:54:53 +0900

    REFACT :: ktlintFormat

--- src/main/kotlin/com/info/maeumgagym/application/domain/pickle/PicklePersistenceAdapter.kt
+++ src/main/kotlin/com/info/maeumgagym/application/domain/pickle/PicklePersistenceAdapter.kt
@@ -1,9 +1,10 @@
 package com.info.maeumgagym.application.domain.pickle
 
-import com.info.maeumgagym.common.annotation.responsibility.PersistenceAdapter
 import com.info.maeumgagym.application.domain.pickle.mapper.PickleMapper
 import com.info.maeumgagym.application.domain.pickle.repository.PickleNativeRepository
 import com.info.maeumgagym.application.domain.pickle.repository.PickleRepository
+import com.info.maeumgagym.common.annotation.responsibility.PersistenceAdapter
+import com.info.maeumgagym.core.pickle.model.Pickle
 import com.info.maeumgagym.core.pickle.port.out.DeletePicklePort
 import com.info.maeumgagym.core.pickle.port.out.ExistsPicklePort
 import com.info.maeumgagym.core.pickle.port.out.ReadPicklePort
@@ -21,22 +22,22 @@ internal class PicklePersistenceAdapter(
 ) : SavePicklePort, DeletePicklePort, ReadPicklePort, ExistsPicklePort {
 
     @Transactional(propagation = Propagation.MANDATORY)
-    override fun save(pickle: com.info.maeumgagym.core.pickle.model.Pickle): com.info.maeumgagym.core.pickle.model.Pickle =
+    override fun save(pickle: Pickle): Pickle =
         pickleMapper.toDomain(
             pickleRepository.save(pickleMapper.toEntity(pickle))
         )
 
     @Transactional(propagation = Propagation.MANDATORY)
-    override fun delete(pickle: com.info.maeumgagym.core.pickle.model.Pickle) {
+    override fun delete(pickle: Pickle) {
         pickleRepository.delete(pickleMapper.toEntity(pickle))
     }
 
-    override fun readById(id: String): com.info.maeumgagym.core.pickle.model.Pickle? =
+    override fun readById(id: String): Pickle? =
         pickleRepository.findById(id)?.let {
             pickleMapper.toDomain(it)
         }
 
-    override fun readAll(): List<com.info.maeumgagym.core.pickle.model.Pickle> =
+    override fun readAll(): List<Pickle> =
         pickleRepository.findAll().map { pickleMapper.toDomain(it) }
 
     override fun readAllPagedByTagsContaining(
@@ -44,7 +45,7 @@ internal class PicklePersistenceAdapter(
         exactTag: String,
         idx: Int,
         size: Int
-    ): MutableList<com.info.maeumgagym.core.pickle.model.Pickle> = prickleNativeRepository.findByTagsContaining(
+    ): MutableList<Pickle> = prickleNativeRepository.findByTagsContaining(
         simpleTag,
         exactTag,
         PageRequest.of(idx, size, Sort.by("created_at").descending())
