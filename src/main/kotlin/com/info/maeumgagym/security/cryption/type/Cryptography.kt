9d787f4db65caa70b986f21b1dc8b086ff4e366a
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-05-05 01:51:54 +0900

    BURN :: 암호화 방식 수정

--- src/main/kotlin/com/info/maeumgagym/security/cryption/type/Cryptography.kt
+++
@@ -1,9 +0,0 @@
-package com.info.maeumgagym.security.cryption.type
-
-enum class Cryptography(
-    val value: String,
-) {
-    HS256("HS256");
-
-    override fun toString(): String = this.value
-}
