0483ed255dca85703117d88d33c3c083df07c647
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-05-04 23:30:39 +0900

    REFACT :: 무효화된 토큰을 저장하기 위한 Redis Entity 추가

---
+++ src/main/kotlin/com/info/maeumgagym/security/authentication/token/revoked/RevokedMGTokenRedisEntity.kt
@@ -0,0 +1,15 @@
+package com.info.maeumgagym.security.authentication.token.revoked
+
+import com.info.maeumgagym.application.TableNames
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+
+@RedisHash(TableNames.REVOKED_MAEUMGAGYM_TOKEN_TABLE)
+class RevokedMGTokenRedisEntity(
+    @Id
+    val id: String,
+
+    @TimeToLive
+    val ttl: Long
+)
