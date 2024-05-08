aa3aad60cff36498aa6a907033f30aabaccb23ef
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-05-08 10:22:52 +0900

    FIX :: Remove Wrong Prefix

--- src/main/kotlin/com/info/maeumgagym/presentation/controller/auth/AuthController.kt
+++ src/main/kotlin/com/info/maeumgagym/presentation/controller/auth/AuthController.kt
@@ -48,7 +48,7 @@ private class AuthController(
         reissueUseCase.reissue(token!!).run {
             val responseHeaders = HttpHeaders().apply {
                 add(HttpHeaders.SET_COOKIE, "RF-TOKEN=$second; Secure; HttpOnly; SameSite=strict")
-                setBearerAuth(first)
+                set(HttpHeaders.AUTHORIZATION, first)
             }
 
             ResponseEntity.ok().headers(responseHeaders).build()
