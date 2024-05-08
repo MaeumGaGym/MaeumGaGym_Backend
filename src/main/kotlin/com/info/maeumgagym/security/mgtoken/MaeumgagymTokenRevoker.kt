a00ff3ee075d5a494b3e5175fccb772b8bd119d5
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-05-08 10:07:35 +0900

    ANNO :: 문서화

--- src/main/kotlin/com/info/maeumgagym/security/mgtoken/MaeumgagymTokenRevoker.kt
+++ src/main/kotlin/com/info/maeumgagym/security/mgtoken/MaeumgagymTokenRevoker.kt
@@ -5,7 +5,15 @@ import com.info.maeumgagym.security.mgtoken.vo.MaeumgagymToken
 /**
  * [MaeumgagymToken]을 무효화.
  *
- * 토큰의 검증 과정은 거치지 않고, 단순히 [tokenId][MaeumgagymToken.tokenId]를 [Redis][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenRepository]를 통해 저장
+ * 토큰의 검증 과정은 거치지 않고, 단순히 [tokenId][MaeumgagymToken.tokenId]를 무효화된 토큰들을 담아두는 [Redis][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenRepository]를 통해 저장
+ *
+ * [RevokedMGTokenContext][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenContext]를 통해 [Redis][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenRepository]에 접근
+ *
+ * @see com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenContext
+ *
+ * @see MaeumgagymTokenEncoder
+ * @see MaeumgagymTokenDecoder
+ * @see MaeumgagymTokenValidator
  *
  * @author Daybreak312
  * @since 04-05-2024
--- src/main/kotlin/com/info/maeumgagym/security/mgtoken/revoked/RevokedMGTokenContext.kt
+++ src/main/kotlin/com/info/maeumgagym/security/mgtoken/revoked/RevokedMGTokenContext.kt
@@ -3,9 +3,9 @@ package com.info.maeumgagym.security.mgtoken.revoked
 import com.info.maeumgagym.security.mgtoken.vo.MaeumgagymToken
 
 /**
- * 무효화된 토큰을 모아둔 컨텍스트
+ * 무효화된 토큰의 [id][MaeumgagymToken.tokenId]을 모아둔 컨텍스트
  *
- * [Redis][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenRepository]를 통해 관리
+ * [Redis][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenRepository]를 통해 관리하며, 저장되는 시간은 토큰의 남은 유효 시간(초) + 100초.
  *
  * @author Daybreak312
  * @since 04-05-2024
@@ -14,13 +14,15 @@ interface RevokedMGTokenContext {
 
     /**
      * 토큰을 무효화 처리
+     *
+     * [토큰 id][MaeumgagymToken.tokenId]를 [Redis][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenRepository]에 저장
      */
     fun revoke(token: MaeumgagymToken)
 
     /**
      * 토큰이 무효화되었는지 확인
      *
-     * 토큰이 만료되었을 경우, 무효화되지 않은 것으로 반환됨
+     * 토큰이 만료되었을 경우, 정확히는 [Redis][com.info.maeumgagym.security.mgtoken.revoked.RevokedMGTokenRepository]에서 삭제되었을 경우 무효화되지 않은 것으로 처리됨
      */
     fun checkRevoked(tokenId: String): Boolean
 }
