9177c6eac0d24dde297c7290ef198bbfb94a1408
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-05-27 20:09:02 +0900

    ANNO :: 주석 재작성

--- src/main/kotlin/com/info/maeumgagym/infrastructure/threadsafe/ThreadValue.kt
+++ src/main/kotlin/com/info/maeumgagym/infrastructure/threadsafe/ThreadValue.kt
@@ -1,11 +1,14 @@
 package com.info.maeumgagym.infrastructure.threadsafe
 
 /**
- * Thread 단위로 값을 사용하기 위한 [ThreadLocal]을 추상화한 클래스
+ * [ThreadLocal]을 Immutable 상태로 만들기 위한 Wrapper
  *
- * 개선점
- * - 어디서나 재설정 가능했던 [ThreadLocal.set]을 생성 시에만 호출하도록 [createWith] 메소드로 감싸 immutable 상태로 수정
- *   더 안정적이며 신뢰성 있는 값을 제공받을 수 있음
+ * 어디서나 접근 가능했던 재설정 메소드([ThreadLocal.set])를 생성 시에만 호출하도록 [createWith] 메소드로 감쌈
+ *
+ * @see ThreadLocal
+ *
+ * @since 27-05-2024
+ * @author Daybreak312
  */
 final class ThreadValue<T> private constructor(
     private val threadLocal: ThreadLocal<T>
