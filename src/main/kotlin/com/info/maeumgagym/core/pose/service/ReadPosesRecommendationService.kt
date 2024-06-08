effbcb39588cc355ee070211ffa84d2fb540081f
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-08 11:37:23 +0900

    REFACT :: 추천 시 분류 기준을 simplePart에서 category로 수정

--- src/main/kotlin/com/info/maeumgagym/core/pose/service/ReadPosesRecommendationService.kt
+++ src/main/kotlin/com/info/maeumgagym/core/pose/service/ReadPosesRecommendationService.kt
@@ -21,17 +21,18 @@ class ReadPosesRecommendationService(
         // 사용자에게 추천할 자세 추출
         readPosePort.readAllRandomLimit30().forEach {
             // 자세의 종류
-            val partName = it.simplePart.first()
-            // 자세의 종류에 해당하는 자세들의 리스트
-            var poses = recommendationPoses[partName]
-            // 만약 이번에 처음으로 자세들을 담은 리스트에 접근한다면
-            if (poses == null) {
-                // 위 주석에서 언급했듯, 새로운 자세 종류이므로 초기화 진행
-                recommendationPoses[partName] = mutableListOf()
-                poses = recommendationPoses[partName]
+            it.category.forEach { partName ->
+                // 자세의 종류에 해당하는 자세들의 리스트
+                var poses = recommendationPoses[partName]
+                // 만약 이번에 처음으로 자세들을 담은 리스트에 접근한다면
+                if (poses == null) {
+                    // 위 주석에서 언급했듯, 새로운 자세 종류이므로 초기화 진행
+                    recommendationPoses[partName] = mutableListOf()
+                    poses = recommendationPoses[partName]
+                }
+                // 자세 리스트에 추가
+                poses!!.add(it)
             }
-            // 자세 리스트에 추가
-            poses!!.add(it)
         }
 
         return PoseRecommendationListResponse(
