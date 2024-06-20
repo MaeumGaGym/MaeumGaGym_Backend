5b374dad1b34de298b9de3ecffb365b28174700d
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-06-20 09:53:45 +0900

    BURN :: useless function

--- src/main/kotlin/com/info/maeumgagym/security/access/checker/AbstractAnnotationBasedUserAuthenticationChecker.kt
+++ src/main/kotlin/com/info/maeumgagym/security/access/checker/AbstractAnnotationBasedUserAuthenticationChecker.kt
@@ -3,7 +3,6 @@ package com.info.maeumgagym.security.access.checker
 import com.info.maeumgagym.common.exception.AuthenticationException
 import org.springframework.security.authentication.AnonymousAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import kotlin.reflect.KClass
 
 /**
  * [AnnotationBasedUserAuthenticationChecker]의 구현체에서 사용되는 일부 중복 코드에 대해 추출된 함수 집합의 추상화
@@ -13,15 +12,8 @@ import kotlin.reflect.KClass
  * @author Daybreak312
  * @since 20-04-2024
  */
-abstract class AbstractAnnotationBasedUserAuthenticationChecker : AnnotationBasedUserAuthenticationChecker {
-
-    protected fun <A : Annotation> Any.getAnnotationOrNull(annotation: KClass<A>): A? {
-        this::class.annotations.forEach {
-            if (it.annotationClass == annotation) return it as A
-        }
-
-        return null
-    }
+abstract class AbstractAnnotationBasedUserAuthenticationChecker
+    : AnnotationBasedUserAuthenticationChecker {
 
     protected fun checkInvalidAuthentication() {
         if (SecurityContextHolder.getContext().authentication == null ||
