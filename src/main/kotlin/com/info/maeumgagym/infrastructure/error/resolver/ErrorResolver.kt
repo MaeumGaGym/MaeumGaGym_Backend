12fb6986a714eaa501d4b82b64c2b4890ffd985b
Daybreak312 <ty82afg12@gmail.com>
2024-07-12 19:00:29 +0900

    ANNO :: ErrorResolver 주석 작성

--- src/main/kotlin/com/info/maeumgagym/infrastructure/error/resolver/ErrorResolver.kt
+++ src/main/kotlin/com/info/maeumgagym/infrastructure/error/resolver/ErrorResolver.kt
@@ -4,6 +4,14 @@ import com.info.maeumgagym.infrastructure.error.vo.ErrorInfo
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+/**
+ * 에러에 대한 처리를 진행
+ *
+ * 여러 개의 하위 구현체가 존재할 수 있음.
+ *
+ * @author Daybreak312
+ * @since 11-07-2024
+ */
 interface ErrorResolver {
 
     fun resolve(
